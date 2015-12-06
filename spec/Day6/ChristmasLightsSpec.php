69d86d6839c0b866e9b47bcdc6cd241b96e3734c
Chris Emerson <chris@cemerson.co.uk>
2015-12-06 13:58:24 +0000

    Day 6 Part I

---
+++ spec/Day6/ChristmasLightsSpec.php
@@ -0,0 +1,82 @@
+<?php
+namespace spec\AdventOfCode\Day6;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ChristmasLightsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('AdventOfCode\Day6\ChristmasLights');
+    }
+
+    function it_should_start_with_all_the_lights_turned_off()
+    {
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(0);
+    }
+
+    function it_can_turn_on_a_single_light()
+    {
+        $this->addInstruction('turn on 0,0 through 0,0');
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(1);
+    }
+
+    function it_can_turn_on_two_lights_after_separate_instructions()
+    {
+        $this->addInstruction('turn on 0,0 through 0,0');
+        $this->addInstruction('turn on 0,1 through 0,1');
+
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(2);
+    }
+
+    function it_can_turn_on_multiple_lights_with_a_single_instruction()
+    {
+        $this->addInstruction('turn on 0,0 through 1,1');
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(4);
+    }
+
+    function it_can_turn_on_a_whole_row_of_lights()
+    {
+        $this->addInstruction('turn on 0,0 through 999,0');
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(1000);
+    }
+
+    function it_can_turn_on_all_the_lights()
+    {
+        $this->addInstruction('turn on 0,0 through 999,999');
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(1000000);
+    }
+
+    function it_can_turn_off_a_single_light()
+    {
+        $this->addInstruction('turn on 0,0 through 0,0');
+        $this->addInstruction('turn off 0,0 through 0,0');
+
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(0);
+    }
+
+    function it_can_toggle_a_light_that_is_off()
+    {
+        $this->addInstruction('toggle 0,0 through 0,0');
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(1);
+    }
+
+    function it_can_toggle_a_light_that_is_on()
+    {
+        $this->addInstruction('turn on 0,0 through 0,0');
+        $this->addInstruction('toggle 0,0 through 0,0');
+
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(0);
+    }
+
+    function it_can_follow_a_complex_series_of_instructions()
+    {
+        $this->addInstruction('turn on 0,0 through 999,999');
+        $this->addInstruction('turn off 2,2 through 997,997');
+        $this->addInstruction('turn on 45,45 through 46,46');
+        $this->addInstruction('toggle 1,1 through 4,4');
+
+        $this->getNumberOfSwitchedOnLights()->shouldReturn(7990);
+    }
+}
---
+++ src/Day6/ChristmasLights.php
@@ -0,0 +1,75 @@
+<?php
+namespace AdventOfCode\Day6;
+
+class ChristmasLights
+{
+    private $lights = [];
+
+    public function __construct()
+    {
+        $row = str_repeat('0', 1000);
+
+        foreach (range(0, 999) as $x) {
+            $this->lights[$x] = $row;
+        }
+    }
+
+    public function addInstruction($instruction)
+    {
+        if (preg_match('/^(?P<instruction>turn (on|off)|toggle) (?P<x1>\d+),(?P<y1>\d+) through (?P<x2>\d+),(?P<y2>\d+)$/', $instruction, $matches)) {
+            list($x1, $x2, $y1, $y2) = $this->getNormalisedValuesFromMatches($matches);
+
+            for ($x = $x1; $x <= $x2; $x++) {
+                for ($y = $y1; $y <= $y2; $y++) {
+                    switch ($matches['instruction']) {
+                        case 'turn on':
+                            $this->turnOn($x, $y);
+                            break;
+
+                        case 'turn off':
+                            $this->turnOff($x, $y);
+                            break;
+
+                        case 'toggle':
+                            $this->toggle($x, $y);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private function getNormalisedValuesFromMatches($matches)
+    {
+        return [
+            min($matches['x1'], $matches['x2']),
+            max($matches['x1'], $matches['x2']),
+            min($matches['y1'], $matches['y2']),
+            max($matches['y1'], $matches['y2'])
+        ];
+    }
+
+    public function getNumberOfSwitchedOnLights()
+    {
+        return array_reduce($this->lights, function($carry, $item) {
+            return $carry + array_sum(str_split($item));
+        });
+    }
+
+    private function turnOn($x, $y)
+    {
+        $this->lights[$x]{$y} = 1;
+    }
+    private function turnOff($x, $y)
+    {
+        $this->lights[$x]{$y} = 0;
+    }
+    private function toggle($x, $y)
+    {
+        if ($this->lights[$x]{$y} == 1) {
+            $this->turnOff($x, $y);
+        } else {
+            $this->turnOn($x, $y);
+        }
+    }
+}
