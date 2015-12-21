3c695accdd50cba0912e118a6b94dd02feb22390
Chris Emerson <chris@cemerson.co.uk>
2015-12-21 08:16:24 +0000

    Day 19 part 2. It works but isn't quick enough!

--- spec/Day19/MedicineMachineSpec.php
+++ spec/Day19/MedicineMachineSpec.php
@@ -59,4 +59,46 @@ class MedicineMachineSpec extends ObjectBehavior
 
         $this->getNumberOfDistinctMolecules()->shouldReturn(1);
     }
+
+    function it_can_find_quickest_route_to_molecule_with_no_steps()
+    {
+        $this->setMolecule('e');
+
+        $this->findQuickestRouteToMolecule('e')->shouldReturn(0);
+    }
+
+    function it_can_find_quickest_route_to_molecule_with_1_transformation()
+    {
+        $this->setMolecule('H');
+
+        $this->addTransformation('e => H');
+
+        $this->findQuickestRouteToMolecule('e')->shouldReturn(1);
+    }
+
+    function it_can_take_multiple_steps_to_get_to_target_molecule()
+    {
+        $this->setMolecule('HOH');
+
+        $this->addTransformation('e => H');
+        $this->addTransformation('e => O');
+        $this->addTransformation('H => HO');
+        $this->addTransformation('H => OH');
+        $this->addTransformation('O => HH');
+
+        $this->findQuickestRouteToMolecule('e')->shouldReturn(3);
+    }
+
+    function it_can_deal_with_a_longer_example()
+    {
+        $this->setMolecule('HOHOHO');
+
+        $this->addTransformation('e => H');
+        $this->addTransformation('e => O');
+        $this->addTransformation('H => HO');
+        $this->addTransformation('H => OH');
+        $this->addTransformation('O => HH');
+
+        $this->findQuickestRouteToMolecule('e')->shouldReturn(6);
+    }
 }
--- src/Day19/MedicineMachine.php
+++ src/Day19/MedicineMachine.php
@@ -26,18 +26,46 @@ class MedicineMachine
     public function getNumberOfDistinctMolecules()
     {
         foreach ($this->transformations as $source => $possibleTransformations) {
-            $offset = 0;
-
-            while (($index = strpos($this->molecule, $source, $offset)) !== false) {
+            for ($offset = 0; ($index = strpos($this->molecule, $source, $offset)) !== false; $offset = $index + 1) {
                 foreach ($possibleTransformations as $possibleTransformation) {
                     $newString = substr_replace($this->molecule, $possibleTransformation, $index, strlen($source));
                     $this->foundTransformations[] = $newString;
                 }
-
-                $offset = $index + 1;
             }
         }
 
         return count(array_unique($this->foundTransformations));
+    }
+
+    public function findQuickestRouteToMolecule($startingMolecule)
+    {
+        return min($this->applyTransformationsToReachTarget($startingMolecule));
+    }
+
+    private function applyTransformationsToReachTarget($currentMolecule, $noSteps = 0)
+    {
+        $return = [];
+
+        if ($currentMolecule == $this->molecule) {
+            $return[] = $noSteps;
+        }
+
+        foreach ($this->transformations as $source => $transformationsForSource) {
+            foreach ($transformationsForSource as $transformation) {
+                $offset = 0;
+
+                while (($index = strpos($currentMolecule, $source, $offset)) !== false) {
+                    $newString = substr_replace($currentMolecule, $transformation, $index, strlen($source));
+
+                    if (strlen($newString) <= strlen($this->molecule)) {
+                        $return = array_merge($return, $this->applyTransformationsToReachTarget($newString, $noSteps + 1));
+                    }
+
+                    $offset = $index + 1;
+                }
+            }
+        }
+
+        return $return;
     }
 }
