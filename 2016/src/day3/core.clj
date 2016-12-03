375141ffbc4c15da2e57de81408d305c1acf789a
Chris Emerson <chris@cemerson.co.uk>
2016-12-03 23:14:46 +0000

    Functions to transpose lines 3 at a time to solve Day 3 part 2

--- 2016/src/day3/core.clj
+++ 2016/src/day3/core.clj
@@ -2,13 +2,21 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
+(defn transpose-lines
+  [[a b c] [d e f] [g h i]]
+  (vector (vector a d g) (vector b e h) (vector c f i)))
+
+(defn rearrange-lines
+  [input transposed]
+  (if (empty? input) transposed (rearrange-lines (drop 3 input) (concat transposed (apply transpose-lines (take 3 input))))))
+
 (defn cleanline
   [line]
   (map (fn [x] (Integer. x)) (filter (fn [x] (not (empty? x))) (map str/trim (str/split line #"\s+")))))
 
 (defn get-instructions
   [filename]
-  (map cleanline (str/split-lines (str/trim (slurp filename)))))
+  (rearrange-lines (map cleanline (str/split-lines (str/trim (slurp filename)))) '()))
 
 (defn triangle?
   [[x y z]]
