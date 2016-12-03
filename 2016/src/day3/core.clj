d9b0bd484ad7214f163741275878e8b997865f39
Chris Emerson <chris@cemerson.co.uk>
2016-12-03 22:14:43 +0000

    Day 3 Part 1 complete

--- 2016/src/day3/core.clj
+++ 2016/src/day3/core.clj
@@ -2,13 +2,19 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
+(defn cleanline
+  [line]
+  (map (fn [x] (Integer. x)) (filter (fn [x] (not (empty? x))) (map str/trim (str/split line #"\s+")))))
+
 (defn get-instructions
   [filename]
-  (str/split-lines (str/trim (slurp filename))))
+  (map cleanline (str/split-lines (str/trim (slurp filename)))))
 
 (defn triangle?
-  [x y z]
-  ())
+  [[x y z]]
+  (let
+    [longestside (max x y z)]
+    (< longestside (- (+ x y z) longestside))))
 
 (defn getvector
   [line]
@@ -16,4 +22,4 @@
 
 (defn -main
   [& args]
-  (println (map getvector (get-instructions (first args)))))
+  (println (count (filter triangle? (get-instructions (first args))))))
