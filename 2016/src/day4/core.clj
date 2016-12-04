52b50946929aa93ea599ba6b88fd25f90add1f50
Chris Emerson <chris@cemerson.co.uk>
2016-12-04 17:19:07 +0000

    Day 4 pt 2

--- 2016/src/day4/core.clj
+++ 2016/src/day4/core.clj
@@ -33,6 +33,29 @@
     [matches (re-matches #"^(?:([a-zA-Z]+)-)+(\d+)\[([a-zA_Z]+)\]$" room)]
     (Integer. (first (take-last 2 matches)))))
 
+(defn decrypt-character
+  [ch sector-id]
+  (if (= \- ch)
+    ch
+    (-> ch
+      (int)
+      (- 97)
+      (+ sector-id)
+      (mod 26)
+      (+ 97)
+      (char))))
+
+(defn decrypt-room-name
+  [room]
+  (let
+    [[_ room-name] (re-matches #"^([a-zA-Z-]+)-(\d+)\[([a-zA_Z]+)\]$" room)
+     sector-id (get-sector-id room)]
+    (apply str (map #(decrypt-character % sector-id) room-name))))
+
 (defn -main
   [& args]
-  (println (apply + (map get-sector-id (filter valid-room? (get-input (first args)))))))
+  (let
+    [filtered-rooms-list (filter valid-room? (get-input (first args)))
+     north-pole-objects-room (first (filter #(str/includes? (decrypt-room-name %) "north") filtered-rooms-list))]
+    (println (apply + (map get-sector-id filtered-rooms-list)))
+    (println "Sector: " (get-sector-id north-pole-objects-room) " Room name: " (decrypt-room-name north-pole-objects-room))))
