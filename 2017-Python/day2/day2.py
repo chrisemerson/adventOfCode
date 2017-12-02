30a0fa177c4bc3776cdeddc0ac2e756c00001b8b
Chris Emerson <chris@cemerson.co.uk>
2017-12-02 21:37:21 +0000

    Day 2 part 2

--- 2017-Python/day2/day2.py
+++ 2017-Python/day2/day2.py
@@ -1,10 +1,17 @@
 spreadsheet = open('input.txt', 'r')
 
 total = 0
+totalpt2 = 0
 
 with spreadsheet as fp:
     for line in fp:
         numbers = list(map(int, line.strip().split("\t")))
         total += max(numbers) - min(numbers)
 
+        for number in numbers:
+            for innernumber in numbers:
+                if number > innernumber and number % innernumber == 0:
+                    totalpt2 += int(number / innernumber)
+
 print(total)
+print(totalpt2)
