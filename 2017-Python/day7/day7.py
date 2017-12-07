ba2e501c8b215e6779c12d67c952fe4375486306
Chris Emerson <Chris.Emerson@bjss.com>
2017-12-07 14:28:43 +0000

    Day 7 Part 2 done

--- 2017-Python/day7/day7.py
+++ 2017-Python/day7/day7.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 programs = {}
 
@@ -9,6 +10,43 @@ def find_root(program_name, programs):
             return find_root(program, programs)
 
     return program_name
+
+
+def get_weight(program_name, programs):
+    total_weight = programs[program_name]['weight']
+
+    if programs[program_name]['children']:
+        for child in programs[program_name]['children']:
+            total_weight += get_weight(child, programs)
+
+    return total_weight
+
+
+def get_child_weights(program_name, programs):
+    child_weights = {}
+
+    if programs[program_name]['children']:
+        for child in programs[program_name]['children']:
+            child_weights[child] = get_weight(child, programs)
+
+    return child_weights
+
+
+def find_unbalanced_element(program_name, programs):
+    child_weights = get_child_weights(program_name, programs)
+    count = Counter(child_weights.values())
+
+    bad_weight = 0
+
+    for index in count:
+        if count[index] == 1:
+            bad_weight = index
+
+    for program in child_weights:
+        if child_weights[program] == bad_weight:
+            return program
+
+    return None
 
 
 with open('input.txt', 'r') as fp:
@@ -23,8 +61,34 @@ with open('input.txt', 'r') as fp:
             for child in childList:
                 children[child] = {}
 
-        programs[matches.group(1)] = {'weight': (matches.group(2)), 'children': children}
+        programs[matches.group(1)] = {'weight': int(matches.group(2)), 'children': children}
 
 root_element = find_root(next(iter(programs.keys())), programs)
 
-print(root_element)
+print("Root element: " + root_element)
+
+unbalanced_element = find_unbalanced_element(root_element, programs)
+unbalanced_element_parent = root_element
+
+while find_unbalanced_element(unbalanced_element, programs) is not None:
+    unbalanced_element_parent = unbalanced_element
+    unbalanced_element = find_unbalanced_element(unbalanced_element, programs)
+
+unbalanced_children_weights = get_child_weights(unbalanced_element_parent, programs)
+current_unbalanced_weight = unbalanced_children_weights[unbalanced_element]
+
+balanced_child_weight = 0
+
+for child in unbalanced_children_weights:
+    if unbalanced_children_weights[child] != current_unbalanced_weight:
+        balanced_child_weight = unbalanced_children_weights[child]
+        break
+
+difference_in_weight_needed = balanced_child_weight - current_unbalanced_weight
+
+print(
+    "Correct weight for element "
+    + unbalanced_element
+    + " should be "
+    + str(programs[unbalanced_element]['weight'] + difference_in_weight_needed)
+)
