4cb280ea3c721d1138cbc0a38c93835c69c494f4
Chris Emerson <Chris.Emerson@bjss.com>
2017-12-28 10:19:06 +0000

    Day 24 part 2

--- 2017-Python/day24/day24.py
+++ 2017-Python/day24/day24.py
@@ -6,7 +6,7 @@ with open('input.txt', 'r') as fp:
         components.append([int(ports[0]), int(ports[1])])
 
 
-def get_longest_bridge(components, open_port):
+def get_strongest_bridge(components, open_port):
     if len(components) == 0:
         return 0
 
@@ -17,14 +17,46 @@ def get_longest_bridge(components, open_port):
             score = components[i][0] + components[i][1]
 
             if components[i][0] == open_port:
-                score += get_longest_bridge(components[:i] + components[i + 1:], components[i][1])
+                score += get_strongest_bridge(components[:i] + components[i + 1:], components[i][1])
             elif components[i][1] == open_port:
-                score += get_longest_bridge(components[:i] + components[i + 1:], components[i][0])
+                score += get_strongest_bridge(components[:i] + components[i + 1:], components[i][0])
 
             if score > max_score:
                 max_score = score
 
     return max_score
+
+
+print(get_strongest_bridge(components, 0))
+
+
+def get_longest_bridge(components, open_port):
+    if len(components) == 0:
+        return (0, 0)
+
+    max_length = 0
+    max_score = 0
+
+    for i in range(0, len(components)):
+        if components[i][0] == open_port or components[i][1] == open_port:
+            score = components[i][0] + components[i][1]
+            length = 1
+
+            if components[i][0] == open_port:
+                result = get_longest_bridge(components[:i] + components[i + 1:], components[i][1])
+            elif components[i][1] == open_port:
+                result = get_longest_bridge(components[:i] + components[i + 1:], components[i][0])
+
+            length += result[0]
+            score += result[1]
+
+            if length > max_length:
+                max_length = length
+                max_score = score
+            elif length == max_length and score > max_score:
+                max_score = score
+
+    return (max_length, max_score)
 
 
 print(get_longest_bridge(components, 0))
