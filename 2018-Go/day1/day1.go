653b6cb34886ad98d96f7ea2e47b594ca54af122
Chris Emerson <Chris.Emerson@bjss.com>
2018-12-05 14:05:13 +0000

    Run go fmt on code so far

--- 2018-Go/day1/day1.go
+++ 2018-Go/day1/day1.go
@@ -1,54 +1,54 @@
 package day1
 
 import (
-    "fmt"
-    "github.com/chrisemerson/AdventOfCode/2018-Go/util"
-    "strconv"
+	"fmt"
+	"github.com/chrisemerson/AdventOfCode/2018-Go/util"
+	"strconv"
 )
 
 func Part1() {
-    input := util.GetInputAsArrayTrimmed("day1/input.txt")
-    freq := 0
+	input := util.GetInputAsArrayTrimmed("day1/input.txt")
+	freq := 0
 
-    for _, v := range input {
-        i, err := strconv.Atoi(v)
+	for _, v := range input {
+		i, err := strconv.Atoi(v)
 
-        if err != nil {
-            panic(err)
-        }
+		if err != nil {
+			panic(err)
+		}
 
-        freq += i
-    }
+		freq += i
+	}
 
-    fmt.Print("Final frequency: ")
-    fmt.Println(freq)
+	fmt.Print("Final frequency: ")
+	fmt.Println(freq)
 }
 
 func Part2() {
-    input := util.GetInputAsArrayTrimmed("day1/input.txt")
-    freq := 0
-    freqs := make(map[int]bool)
+	input := util.GetInputAsArrayTrimmed("day1/input.txt")
+	freq := 0
+	freqs := make(map[int]bool)
 
-    freqs[0] = true
+	freqs[0] = true
 
-    for true {
-        for _, v := range input {
-            i, err := strconv.Atoi(v)
+	for true {
+		for _, v := range input {
+			i, err := strconv.Atoi(v)
 
-            if err != nil {
-                panic(err)
-            }
+			if err != nil {
+				panic(err)
+			}
 
-            freq += i
+			freq += i
 
-            if freqs[freq] {
-                fmt.Print("First frequency reached twice: ")
-                fmt.Println(freq)
+			if freqs[freq] {
+				fmt.Print("First frequency reached twice: ")
+				fmt.Println(freq)
 
-                return
-            }
+				return
+			}
 
-            freqs[freq] = true
-        }
-    }
+			freqs[freq] = true
+		}
+	}
 }
