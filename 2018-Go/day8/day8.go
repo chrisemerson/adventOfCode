86ec33102477e7dc657425142339777f3703303d
Chris Emerson <chris@cemerson.co.uk>
2018-12-15 12:46:39 +0000

    Day 8 both parts... finally!

--- 2018-Go/day8/day8.go
+++ 2018-Go/day8/day8.go
@@ -2,15 +2,14 @@ package day8
 
 import (
 	"fmt"
+	"github.com/chrisemerson/AdventOfCode/2018-Go/util"
+	"sort"
 	"strconv"
 	"strings"
-	"github.com/chrisemerson/AdventOfCode/2018-Go/util"
 )
 
 func Part1() {
 	nodes := getInput()
-
-	fmt.Print(nodes)
 
 	metaDataTotal := 0
 
@@ -25,6 +24,16 @@ func Part1() {
 }
 
 func Part2() {
+	nodes := getInput()
+
+	nodeValue := getNodeValue(1, nodes)
+
+	if nodeValue == 33 {
+		fmt.Println(nodes)
+	}
+
+	fmt.Print("Root node value: ")
+	fmt.Println(nodeValue)
 }
 
 func getInput() map[int]map[string][]int {
@@ -46,7 +55,7 @@ func getInput() map[int]map[string][]int {
 
 	label := 1
 
-	childNodes, remainingData := returnNodes(dataInt[2:], noChildren, label + 1)
+	childNodes, remainingData := returnNodes(dataInt[2:], noChildren, label+1)
 
 	alreadyParented := []int{}
 
@@ -94,25 +103,30 @@ func returnNodes(data []int, children int, label int) (map[int]map[string][]int,
 
 		if noChildren == 0 {
 			for i := 0; i < noMetadata; i++ {
-				metadata := remainingData[i + 2]
+				metadata := remainingData[i+2]
 				node["metadata"] = append(node["metadata"], metadata)
 			}
 
 			nodes[label] = node
 
-			remainingData = remainingData[2 + noMetadata:]
+			remainingData = remainingData[2+noMetadata:]
 		} else {
 			node["children"] = []int{}
 			node["metadata"] = []int{}
 
 			childNodes := make(map[int]map[string][]int, 0)
 
-			childNodes, remainingData = returnNodes(remainingData[2:], noChildren, label + 1)
+			childNodes, remainingData = returnNodes(remainingData[2:], noChildren, label+1)
 
 			alreadyParented := []int{}
+			maxChildLabel := 0
 
-			for _, childNode := range childNodes {
+			for childLabel, childNode := range childNodes {
 				alreadyParented = append(alreadyParented, childNode["children"]...)
+
+				if childLabel > maxChildLabel {
+					maxChildLabel = childLabel
+				}
 			}
 
 			for childLabel, childNode := range childNodes {
@@ -138,10 +152,32 @@ func returnNodes(data []int, children int, label int) (map[int]map[string][]int,
 			remainingData = remainingData[noMetadata:]
 
 			nodes[label] = node
+
+			label = maxChildLabel
 		}
 
 		label += 1
 	}
 
 	return nodes, remainingData
+}
+
+func getNodeValue(node int, nodes map[int]map[string][]int) int {
+	totalValue := 0
+	childNodes := nodes[node]["children"]
+	sort.Ints(childNodes)
+
+	if len(nodes[node]["children"]) == 0 {
+		for i := 0; i < len(nodes[node]["metadata"]); i++ {
+			totalValue += nodes[node]["metadata"][i]
+		}
+	} else {
+		for i := 0; i < len(nodes[node]["metadata"]); i++ {
+			if nodes[node]["metadata"][i] != 0 && nodes[node]["metadata"][i] <= len(childNodes) {
+				totalValue += getNodeValue(childNodes[nodes[node]["metadata"][i]-1], nodes)
+			}
+		}
+	}
+
+	return totalValue
 }
