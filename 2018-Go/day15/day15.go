ed4a189ee113e7e6accbd34e6c6464222306ac96
Chris Emerson <chris@cemerson.co.uk>
2018-12-15 22:09:05 +0000

    Day 15 both parts complete

--- 2018-Go/day15/day15.go
+++ 2018-Go/day15/day15.go
@@ -6,10 +6,51 @@ import (
 )
 
 func Part1() {
-	board, units := getInput()
+	round, hitPointSum, _ := runBattle(3)
+
+	fmt.Print("Game over, outcome: ")
+	fmt.Print(round)
+	fmt.Print(" rounds completed, ")
+	fmt.Print(hitPointSum)
+	fmt.Print(" hitpoints left, total: ")
+	fmt.Println(round * hitPointSum)
+}
+
+func Part2() {
+	elvesDead := true
+
+	//Played with this manually to find the right kind of area before letting the program run so it didn't take forever
+	elvesAttackPower := 19
+	round := 0
+	hitPointSum := 0
+
+	for elvesDead {
+		elvesAttackPower++
+
+		fmt.Print("Running attack with power of ")
+		fmt.Println(elvesAttackPower)
+
+		round, hitPointSum, elvesDead = runBattle(elvesAttackPower)
+	}
+
+	fmt.Print("Elves need an attack power of ")
+	fmt.Print(elvesAttackPower)
+	fmt.Println(" to win with no deaths")
+
+	fmt.Print("Game over, outcome: ")
+	fmt.Print(round)
+	fmt.Print(" rounds completed, ")
+	fmt.Print(hitPointSum)
+	fmt.Print(" hitpoints left, total: ")
+	fmt.Println(round * hitPointSum)
+}
+
+func runBattle(elvesAttackPower int) (int, int, bool) {
+	board, units := getInput(elvesAttackPower)
 
 	round := 0
 	gameOver := false
+	elvesDead := false
 
 	for !gameOver {
 		//New Round
@@ -19,25 +60,25 @@ func Part1() {
 		//Each unit plays in a round in 'reading' order
 		for !allUnitsMovedThisRound {
 			//First, pick a unit to move...
+			unitToMove := -1
 
-			unitToMove := 0
-
-			for y, line := range board {
-				for x := range line {
+			for y := 0; y < len(board); y++ {
+				for x := 0; x < len(board[y]); x++ {
 					for idx, unit := range units {
-						if unit["x"] == x && unit["y"] == y && unit["played"] == 0 && unitToMove == 0 {
+						if unit["x"] == x && unit["y"] == y && unit["played"] == 0 && unitToMove == -1 {
 							unitToMove = idx
 						}
 					}
 				}
 			}
 
+			units[unitToMove]["played"] = 1
+
 			//First, is there an enemy in range?
 			inRange := enemiesInRange(units, units[unitToMove])
 
 			if len(inRange) == 0 {
 				//Need to move first
-
 				//Find Targets
 				targets := []int{}
 
@@ -50,9 +91,10 @@ func Part1() {
 				//Find spaces In Range based on targets
 				inRangeSpaces := make([]map[string]int, 0)
 				obstaclesBoard := getObstaclesBoard(board, units)
+				obstaclesBoard[units[unitToMove]["y"]][units[unitToMove]["x"]] = "."
 
 				for _, target := range targets {
-					if obstaclesBoard[units[target]["y"] - 1][units[target]["x"]] == "." {
+					if obstaclesBoard[units[target]["y"]-1][units[target]["x"]] == "." {
 						inRangeSpace := make(map[string]int, 0)
 
 						inRangeSpace["y"] = units[target]["y"] - 1
@@ -61,7 +103,7 @@ func Part1() {
 						inRangeSpaces = append(inRangeSpaces, inRangeSpace)
 					}
 
-					if obstaclesBoard[units[target]["y"]][units[target]["x"] - 1] == "." {
+					if obstaclesBoard[units[target]["y"]][units[target]["x"]-1] == "." {
 						inRangeSpace := make(map[string]int, 0)
 
 						inRangeSpace["y"] = units[target]["y"]
@@ -70,7 +112,7 @@ func Part1() {
 						inRangeSpaces = append(inRangeSpaces, inRangeSpace)
 					}
 
-					if obstaclesBoard[units[target]["y"]][units[target]["x"] + 1] == "." {
+					if obstaclesBoard[units[target]["y"]][units[target]["x"]+1] == "." {
 						inRangeSpace := make(map[string]int, 0)
 
 						inRangeSpace["y"] = units[target]["y"]
@@ -79,7 +121,7 @@ func Part1() {
 						inRangeSpaces = append(inRangeSpaces, inRangeSpace)
 					}
 
-					if obstaclesBoard[units[target]["y"] + 1][units[target]["x"]] == "." {
+					if obstaclesBoard[units[target]["y"]+1][units[target]["x"]] == "." {
 						inRangeSpace := make(map[string]int, 0)
 
 						inRangeSpace["y"] = units[target]["y"] + 1
@@ -90,13 +132,11 @@ func Part1() {
 				}
 
 				for idx, inRangeSpace := range inRangeSpaces {
-					path, reachable := findShortestPath(obstaclesBoard, units[unitToMove]["x"], units[unitToMove]["y"], inRangeSpace["x"], inRangeSpace["y"])
+					distanceBoard := findShortestPath(obstaclesBoard, units[unitToMove]["x"], units[unitToMove]["y"], inRangeSpace["x"], inRangeSpace["y"])
 
-					if reachable {
+					if distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]] != -1 {
 						inRangeSpaces[idx]["reachable"] = 1
-						inRangeSpaces[idx]["pathlen"] = len(path)
-						inRangeSpaces[idx]["movex"] = path[0]["x"]
-						inRangeSpaces[idx]["movey"] = path[0]["y"]
+						inRangeSpaces[idx]["pathlen"] = distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]]
 					} else {
 						inRangeSpaces[idx]["reachable"] = 0
 					}
@@ -115,8 +155,8 @@ func Part1() {
 				moveTowards := -1
 
 				//Reading order...
-				for y, row := range board {
-					for x := range row {
+				for y := 0; y < len(board); y++ {
+					for x := 0; x < len(board[y]); x++ {
 						for idx, inRangeSpace := range inRangeSpaces {
 							if inRangeSpace["x"] == x && inRangeSpace["y"] == y && inRangeSpace["pathlen"] == shortestPath && moveTowards == -1 {
 								moveTowards = idx
@@ -126,8 +166,17 @@ func Part1() {
 				}
 
 				if moveTowards != -1 {
-					units[unitToMove]["x"] = inRangeSpaces[moveTowards]["movex"]
-					units[unitToMove]["y"] = inRangeSpaces[moveTowards]["movey"]
+					distanceBoard := findShortestPath(obstaclesBoard, units[unitToMove]["x"], units[unitToMove]["y"], inRangeSpaces[moveTowards]["x"], inRangeSpaces[moveTowards]["y"])
+
+					if distanceBoard[units[unitToMove]["y"]-1][units[unitToMove]["x"]] == distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]]-1 {
+						units[unitToMove]["y"] = units[unitToMove]["y"] - 1
+					} else if distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]-1] == distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]]-1 {
+						units[unitToMove]["x"] = units[unitToMove]["x"] - 1
+					} else if distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]+1] == distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]]-1 {
+						units[unitToMove]["x"] = units[unitToMove]["x"] + 1
+					} else if distanceBoard[units[unitToMove]["y"]+1][units[unitToMove]["x"]] == distanceBoard[units[unitToMove]["y"]][units[unitToMove]["x"]]-1 {
+						units[unitToMove]["y"] = units[unitToMove]["y"] + 1
+					}
 				}
 			}
 
@@ -138,35 +187,41 @@ func Part1() {
 				//Attack!
 
 				//First, pick a target - lowest hitpoints wins
-				lowestHP := 200
+				lowestHP := 201
 
-				for idx := range inRange {
+				for _, idx := range inRange {
 					if units[idx]["hitpoints"] < lowestHP {
 						lowestHP = units[idx]["hitpoints"]
 					}
 				}
 
-				enemyToAttack := 0
+				enemyToAttack := -1
 
 				//In reading order
-				for y, line := range board {
-					for x := range line {
-						for idx := range inRange {
+				for y := 0; y < len(board); y++ {
+					for x := 0; x < len(board[y]); x++ {
+						for _, idx := range inRange {
 							//If we don't already have an enemy to attack and the hitpoints matches the lowest we found...
-							if units[idx]["x"] == x && units[idx]["y"] == y && enemyToAttack == 0 && units[idx]["hitpoints"] == lowestHP {
+							if units[idx]["x"] == x && units[idx]["y"] == y && enemyToAttack == -1 && units[idx]["hitpoints"] == lowestHP {
 								enemyToAttack = idx
 							}
 						}
 					}
 				}
 
-				//Attack the enemy...
-				units[enemyToAttack]["hitpoints"] = units[enemyToAttack]["hitpoints"] - units[unitToMove]["attack"]
+				if enemyToAttack != -1 {
+					//Attack the enemy...
+					units[enemyToAttack]["hitpoints"] = units[enemyToAttack]["hitpoints"] - units[unitToMove]["attack"]
 
-				if units[enemyToAttack]["hitpoints"] <= 0 {
-					//He be dead... remove from the game
+					if units[enemyToAttack]["hitpoints"] <= 0 {
+						//He be dead... remove from the game
 
-					units = append(units[:enemyToAttack], units[enemyToAttack + 1:]...)
+						if units[enemyToAttack]["team"] == 0 {
+							elvesDead = true
+						}
+
+						units = append(units[:enemyToAttack], units[enemyToAttack+1:]...)
+					}
 				}
 			}
 
@@ -178,22 +233,27 @@ func Part1() {
 					allUnitsMovedThisRound = false
 				}
 			}
+
+			//Check if game over
+			teamsStillInGame := make(map[int]bool, 0)
+
+			for _, unit := range units {
+				teamsStillInGame[unit["team"]] = true
+			}
+
+			if len(teamsStillInGame) == 1 {
+				gameOver = true
+
+				if allUnitsMovedThisRound {
+					//Count only FULL rounds
+					round -= 1
+				}
+			}
 		}
 
 		//Reset 'played' bit on all remaining units
 		for idx := range units {
 			units[idx]["played"] = 0
-		}
-
-		//Check if game over
-		teamsStillInGame := make(map[int]bool, 0)
-
-		for _, unit := range units {
-			teamsStillInGame[unit["team"]] = true
-		}
-
-		if len(teamsStillInGame) == 1 {
-			gameOver = true
 		}
 	}
 
@@ -203,14 +263,10 @@ func Part1() {
 		hitPointSum += unit["hitpoints"]
 	}
 
-	fmt.Print("Game over, outcome: ")
-	fmt.Println(round * hitPointSum)
+	return round, hitPointSum, elvesDead
 }
 
-func Part2() {
-}
-
-func getInput() (map[int]map[int]string, []map[string]int) {
+func getInput(elvesAttackPower int) (map[int]map[int]string, []map[string]int) {
 	input := util.GetInputAsArrayTrimmed("day15/input.txt")
 
 	board := make(map[int]map[int]string, 0)
@@ -230,13 +286,14 @@ func getInput() (map[int]map[int]string, []map[string]int) {
 
 				if char == 'E' {
 					unit["team"] = 0
+					unit["attack"] = elvesAttackPower
 				} else {
 					unit["team"] = 1
+					unit["attack"] = 3
 				}
 
 				unit["played"] = 0
 				unit["hitpoints"] = 200
-				unit["attack"] = 3
 
 				units = append(units, unit)
 			} else {
@@ -248,15 +305,15 @@ func getInput() (map[int]map[int]string, []map[string]int) {
 	return board, units
 }
 
-func enemiesInRange (units []map[string]int, unit map[string]int) []int {
+func enemiesInRange(units []map[string]int, unit map[string]int) []int {
 	enemiesInRange := []int{}
 
 	for idx, thisUnit := range units {
-		if thisUnit["team"] != unit["team"] && thisUnit["y"] == unit["y"] && (thisUnit["x"] + 1 == unit["x"] || thisUnit["x"] - 1 == unit["x"]) {
+		if thisUnit["team"] != unit["team"] && thisUnit["y"] == unit["y"] && (thisUnit["x"]+1 == unit["x"] || thisUnit["x"]-1 == unit["x"]) {
 			enemiesInRange = append(enemiesInRange, idx)
 		}
 
-		if thisUnit["team"] != unit["team"] && thisUnit["x"] == unit["x"] && (thisUnit["y"] + 1 == unit["y"] || thisUnit["y"] - 1 == unit["y"]) {
+		if thisUnit["team"] != unit["team"] && thisUnit["x"] == unit["x"] && (thisUnit["y"]+1 == unit["y"] || thisUnit["y"]-1 == unit["y"]) {
 			enemiesInRange = append(enemiesInRange, idx)
 		}
 	}
@@ -267,10 +324,10 @@ func enemiesInRange (units []map[string]int, unit map[string]int) []int {
 func getObstaclesBoard(board map[int]map[int]string, units []map[string]int) map[int]map[int]string {
 	obstaclesBoard := make(map[int]map[int]string, 0)
 
-	for y, row := range board {
+	for y := 0; y < len(board); y++ {
 		obstaclesBoard[y] = make(map[int]string, 0)
 
-		for x, cell := range row {
+		for x := 0; x < len(board[y]); x++ {
 			unitHere := false
 
 			for _, unit := range units {
@@ -282,7 +339,7 @@ func getObstaclesBoard(board map[int]map[int]string, units []map[string]int) map
 			if unitHere {
 				obstaclesBoard[y][x] = "#"
 			} else {
-				obstaclesBoard[y][x] = cell
+				obstaclesBoard[y][x] = board[y][x]
 			}
 		}
 	}
@@ -290,96 +347,57 @@ func getObstaclesBoard(board map[int]map[int]string, units []map[string]int) map
 	return obstaclesBoard
 }
 
-func findShortestPath(board map[int]map[int]string, srcx int, srcy int, dstx int, dsty int) ([]map[string]int, bool) {
-	path := make([]map[string]int, 0)
-	maxPathLen := len(board) * len(board[0])
+func findShortestPath(board map[int]map[int]string, srcx int, srcy int, dstx int, dsty int) map[int]map[int]int {
+	distanceBoard := make(map[int]map[int]int, 0)
 
-	if srcx == dstx && srcy == dsty {
-		return path, true
-	}
+	for y := 0; y < len(board); y++ {
+		distanceBoard[y] = make(map[int]int, 0)
 
-	//Find shortest path in each direction from current 'end' spot
-	possiblePaths := make([][]map[string]int, 0)
-	newCoord := make(map[string]int, 0)
-
-	if board[srcx][srcy - 1] == "." {
-		upPath, upBlocked := findShortestPath(board, srcx, srcy - 1, dstx, dsty)
-
-		newCoord["x"] = srcx
-		newCoord["y"] = srcy - 1
-
-		path = append(path, newCoord)
-
-		if !upBlocked && !checkDoubleBack(newCoord, upPath) && len(upPath) < maxPathLen {
-			possiblePaths = append(possiblePaths, append(path, upPath...))
-		}
-	}
-
-	if board[srcx - 1][srcy] == "." {
-		leftPath, leftBlocked := findShortestPath(board, srcx - 1, srcy, dstx, dsty)
-
-		newCoord["x"] = srcx - 1
-		newCoord["y"] = srcy
-
-		path = append(path, newCoord)
-
-		if !leftBlocked && !checkDoubleBack(newCoord, leftPath) && len(leftPath) < maxPathLen {
-			possiblePaths = append(possiblePaths, append(path, leftPath...))
-		}
-	}
-
-	if board[srcx + 1][srcy] == "." {
-		rightPath, rightBlocked := findShortestPath(board, srcx + 1, srcy, dstx, dsty)
-
-		newCoord["x"] = srcx + 1
-		newCoord["y"] = srcy
-
-		path = append(path, newCoord)
-
-		if !rightBlocked && !checkDoubleBack(newCoord, rightPath) && len(rightPath) < maxPathLen {
-			possiblePaths = append(possiblePaths, append(path, rightPath...))
-		}
-	}
-
-	if board[srcx][srcy + 1] == "." {
-		downPath, downBlocked := findShortestPath(board, srcx, srcy + 1, dstx, dsty)
-
-		newCoord["x"] = srcx
-		newCoord["y"] = srcy + 1
-
-		path = append(path, newCoord)
-
-		if !downBlocked && !checkDoubleBack(newCoord, downPath) && len(downPath) < maxPathLen {
-			possiblePaths = append(possiblePaths, append(path, downPath...))
-		}
-	}
-
-	shortestPath := maxPathLen
-
-	if len(possiblePaths) > 1 {
-		for _, possiblePath := range possiblePaths {
-			if len(possiblePath) < shortestPath {
-				shortestPath = len(possiblePath)
-			}
-		}
-
-		for _, possiblePath := range possiblePaths {
-			if len(possiblePath) == shortestPath {
-				return possiblePath, true
+		for x := 0; x < len(board[y]); x++ {
+			if board[y][x] == "." {
+				distanceBoard[y][x] = -1
+			} else {
+				distanceBoard[y][x] = 99999
 			}
 		}
 	}
 
+	distanceBoard[dsty][dstx] = 0
+	level := 1
+	spacePainted := true
 
-	return path, false
-}
+	for spacePainted {
+		spacePainted = false
 
-func checkDoubleBack(coord map[string]int, path []map[string]int) bool {
-	for _, pathCoord := range path {
-		if pathCoord["x"] == coord["x"] && pathCoord["y"] == coord["y"] {
-			return true
+		for y := 0; y < len(distanceBoard); y++ {
+			for x := 0; x < len(distanceBoard[y]); x++ {
+
+				if distanceBoard[y][x] == level-1 {
+					if distanceBoard[y-1][x] == -1 {
+						distanceBoard[y-1][x] = level
+						spacePainted = true
+					}
+
+					if distanceBoard[y][x-1] == -1 {
+						distanceBoard[y][x-1] = level
+						spacePainted = true
+					}
+
+					if distanceBoard[y][x+1] == -1 {
+						distanceBoard[y][x+1] = level
+						spacePainted = true
+					}
+
+					if distanceBoard[y+1][x] == -1 {
+						distanceBoard[y+1][x] = level
+						spacePainted = true
+					}
+				}
+			}
 		}
+
+		level += 1
 	}
 
-	return false
+	return distanceBoard
 }
