b4b3a0687a29e879f41b824a88c66ba64cdddb74
Chris Emerson <chris@cemerson.co.uk>
2019-12-04 07:39:54 +0000

    Refactor Day 4

--- 2019-Kotlin/src/uk/co/cemerson/aoc/Day4/Day4.kt
+++ 2019-Kotlin/src/uk/co/cemerson/aoc/Day4/Day4.kt
@@ -4,29 +4,38 @@ import uk.co.cemerson.aoc.AOCDay
 
 class Day4 : AOCDay {
     override fun part1() {
-        val passwords = (172930..683082)
-                .map { it.toString() }
-                .filter { it[0] == it[1] || it[1] == it[2] || it[2] == it[3] || it[3] == it[4] || it[4] == it[5] }
-                .filter { it[0] <= it[1] && it[1] <= it[2] && it[2] <= it[3] && it[3] <= it[4] && it[4] <= it[5] }
+        val passwords = getInputRange()
+                .filter { it.hasDigitsInAscendingOrEqualOrder() }
+                .filter { it.hasAtLeastTwoAdjacentIdenticalDigits() }
                 .count()
 
         println("Number of passwords: " + passwords)
     }
 
     override fun part2() {
-        val passwords = (172930..683082)
-                .map { it.toString() }
-                .filter {
-                    (it[0] == it[1] && it [1] != it[2])
-                            || (it[0] != it[1] && it[1] == it[2] && it [2] != it[3])
-                            || (it[1] != it[2] && it[2] == it[3] && it [3] != it[4])
-                            || (it[2] != it[3] && it[3] == it[4] && it [4] != it[5])
-                            || (it[3] != it[4] && it[4] == it[5])
-                }
-                .filter { it[0] <= it[1] && it[1] <= it[2] && it[2] <= it[3] && it[3] <= it[4] && it[4] <= it[5] }
+        val passwords = getInputRange()
+                .filter { it.hasDigitsInAscendingOrEqualOrder() }
+                .filter { it.hasExactlyTwoAdjacentIdenticalDigits() }
                 .count()
 
         println("Number of passwords: " + passwords)
     }
+
+    private fun getInputRange() = (172930..683082)
+            .map { it.toString() }
+
+    private fun String.hasDigitsInAscendingOrEqualOrder() =
+            this[0] <= this[1] && this[1] <= this[2] && this[2] <= this[3] && this[3] <= this[4] && this[4] <= this[5]
+
+    private fun String.hasExactlyTwoAdjacentIdenticalDigits(): Boolean {
+        return ((this[0] == this[1] && this[1] != this[2])
+                || (this[0] != this[1] && this[1] == this[2] && this[2] != this[3])
+                || (this[1] != this[2] && this[2] == this[3] && this[3] != this[4])
+                || (this[2] != this[3] && this[3] == this[4] && this[4] != this[5])
+                || (this[3] != this[4] && this[4] == this[5]))
+    }
+
+    private fun String.hasAtLeastTwoAdjacentIdenticalDigits() =
+            this[0] == this[1] || this[1] == this[2] || this[2] == this[3] || this[3] == this[4] || this[4] == this[5]
 }
 
