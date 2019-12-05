285780affe3c3c339976c5ac9d6ca5869e1bb63d
Chris Emerson <Chris.Emerson@bjss.com>
2019-12-05 12:38:39 +0000

    Small Day 4 refactor

--- 2019-Kotlin/src/uk/co/cemerson/aoc/Day4/Day4.kt
+++ 2019-Kotlin/src/uk/co/cemerson/aoc/Day4/Day4.kt
@@ -21,19 +21,20 @@ class Day4 : AOCDay {
         println("Number of passwords: " + passwords)
     }
 
-    private fun getInputRange() = (172930..683082)
-            .map { it.toString() }
+    private fun getInputRange() =
+            (172930..683082)
+                    .map { it.toString() }
 
     private fun String.hasDigitsInAscendingOrEqualOrder() =
             this[0] <= this[1] && this[1] <= this[2] && this[2] <= this[3] && this[3] <= this[4] && this[4] <= this[5]
 
-    private fun String.hasExactlyTwoAdjacentIdenticalDigits(): Boolean {
-        return ((this[0] == this[1] && this[1] != this[2])
-                || (this[0] != this[1] && this[1] == this[2] && this[2] != this[3])
-                || (this[1] != this[2] && this[2] == this[3] && this[3] != this[4])
-                || (this[2] != this[3] && this[3] == this[4] && this[4] != this[5])
-                || (this[3] != this[4] && this[4] == this[5]))
-    }
+    private fun String.hasExactlyTwoAdjacentIdenticalDigits(): Boolean =
+            ((this[0] == this[1] && this[1] != this[2])
+                    || (this[0] != this[1] && this[1] == this[2] && this[2] != this[3])
+                    || (this[1] != this[2] && this[2] == this[3] && this[3] != this[4])
+                    || (this[2] != this[3] && this[3] == this[4] && this[4] != this[5])
+                    || (this[3] != this[4] && this[4] == this[5]))
+
 
     private fun String.hasAtLeastTwoAdjacentIdenticalDigits() =
             this[0] == this[1] || this[1] == this[2] || this[2] == this[3] || this[3] == this[4] || this[4] == this[5]
