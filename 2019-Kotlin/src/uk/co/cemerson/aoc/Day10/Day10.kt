ffb25650230703e46817dd5f754fafa8222a96c9
Chris Emerson <Chris.Emerson@bjss.com>
2019-12-10 10:47:22 +0000

    Tidy up Day 10

--- 2019-Kotlin/src/uk/co/cemerson/aoc/Day10/Day10.kt
+++ 2019-Kotlin/src/uk/co/cemerson/aoc/Day10/Day10.kt
@@ -3,13 +3,11 @@ package uk.co.cemerson.aoc.Day10
 import uk.co.cemerson.aoc.AOCDay
 import java.lang.Math.abs
 import java.lang.Math.sqrt
+import java.util.Comparator
 
 class Day10 : AOCDay {
     override fun part1() {
-        val asteroids = getAsteroidsCoords()
-
-        val visibleAsteroidCount = asteroids
-                .map { Triple(it.first, it.second, countAsteroidsVisibleFromPoint(it, asteroids)) }
+        val visibleAsteroidCount = getListOfAsteroidsWithVisibleCount()
 
         val maxVisibleAsteroids = visibleAsteroidCount
                 .map { it.third }
@@ -23,76 +21,22 @@ class Day10 : AOCDay {
     }
 
     override fun part2() {
-        val asteroidBase = Pair(26, 29)
-        val destroyedAsteroids = returnAsteroidDestructionOrder(getAsteroidsCoords(), asteroidBase)
+        val asteroidBase =
+                getListOfAsteroidsWithVisibleCount()
+                        .sortedBy { it.third }
+                        .takeLast(1)
+                        .map { Pair(it.first, it.second) }
+                        .first()
 
-        val asteroid200 = destroyedAsteroids.take(200).last()
+        val asteroid200 =
+                getAsteroidDestructionOrder(
+                        getAsteroidsCoords(),
+                        asteroidBase
+                )
+                        .take(200)
+                        .last()
 
         println("Answer: " + (asteroid200.first * 100 + asteroid200.second))
-    }
-
-    private fun returnAsteroidDestructionOrder(asteroids: List<Pair<Int, Int>>, base: Pair<Int, Int>): List<Pair<Int, Int>> {
-        val distances = asteroids
-                .map { Pair(it.first - base.first, it.second - base.second) }
-                .filter { it.first != 0 || it.second != 0 }
-
-        val topRightAsteroids = distances
-                .filter { it.first >= 0 && it.second < 0 }
-                .map { Pair(it, Pair(it.first.toDouble() / it.second.toDouble(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble()))) }
-                .groupBy { it.second.first }
-                .toSortedMap(reverseOrder())
-                .map { (_, info) -> info.minBy { it.second.second }!! }
-                .map { it.first }
-
-        val rightAsteroid = distances
-                .filter { it.first > 0 && it.second == 0 }
-                .minBy { abs(it.first) }
-
-        val bottomRightAsteroids = distances
-                .filter { it.first >= 0 && it.second > 0 }
-                .map { Pair(it, Pair(it.first.toDouble() / it.second.toDouble(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble()))) }
-                .groupBy { it.second.first }
-                .toSortedMap()
-                .map { (_, info) -> info.minBy { it.second.second }!! }
-                .map { it.first }
-
-        val bottomLeftAsteroids = distances
-                .filter { it.first < 0 && it.second > 0 }
-                .map { Pair(it, Pair(it.first.toDouble() / it.second.toDouble(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble()))) }
-                .groupBy { it.second.first }
-                .toSortedMap()
-                .map { (_, info) -> info.minBy { it.second.second }!! }
-                .map { it.first }
-
-        val leftAsteroid = distances
-                .filter { it.first < 0 && it.second == 0 }
-                .minBy { abs(it.first) }
-
-        val topLeftAsteroids = distances
-                .filter { it.first < 0 && it.second < 0 }
-                .map { Pair(it, Pair(it.first.toDouble() / it.second.toDouble(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble()))) }
-                .groupBy { it.second.first }
-                .toSortedMap(reverseOrder())
-                .map { (_, info) -> info.minBy { it.second.second }!! }
-                .map { it.first }
-
-        var destroyedAsteroids = topRightAsteroids
-
-        if (rightAsteroid != null) {
-            destroyedAsteroids = destroyedAsteroids + rightAsteroid
-        }
-
-        destroyedAsteroids = destroyedAsteroids + bottomRightAsteroids + bottomLeftAsteroids
-
-        if (leftAsteroid != null) {
-            destroyedAsteroids = destroyedAsteroids + leftAsteroid
-        }
-
-        destroyedAsteroids = destroyedAsteroids + topLeftAsteroids
-
-        //Filter out destroyed asteroids and call recursively for completeness
-
-        return destroyedAsteroids.map { Pair(it.first + base.first, it.second + base.second) }
     }
 
     private fun getAsteroidsCoords(): MutableList<Pair<Int, Int>> {
@@ -106,31 +50,101 @@ class Day10 : AOCDay {
                 }
             }
         }
+
         return asteroids
     }
 
-    private fun countAsteroidsVisibleFromPoint(point: Pair<Int, Int>, asteroids: MutableList<Pair<Int, Int>>): Int {
-        val distances = asteroids
-                .map { Pair(it.first - point.first, it.second - point.second) }
-                .filter { it.first != 0 || it.second != 0 }
+    private fun getListOfAsteroidsWithVisibleCount(): List<Triple<Int, Int, Int>> {
+        val asteroids = getAsteroidsCoords()
 
-        val sameLineAsteroids = distances
+        return asteroids
+                .map { Triple(it.first, it.second, countAsteroidsVisibleFromPoint(it, asteroids)) }
+    }
+
+    private fun countAsteroidsVisibleFromPoint(point: Pair<Int, Int>, asteroids: MutableList<Pair<Int, Int>>): Int {
+        val relativeAsteroidPositions = getRelativeAsteroidPositions(asteroids, point)
+
+        return (relativeAsteroidPositions
                 .filter { it.second == 0 }
                 .groupBy { it.first > 0 }
                 .count()
-
-        val topAsteroids = distances
-                .filter { it.second < 0 }
-                .map { Pair(it.first.toDouble() / it.second.toDouble(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble())) }
-                .groupBy { it.first }
-                .count()
-
-        val bottomAsteroids = distances
-                .filter { it.second > 0 }
-                .map { Pair(it.first.toFloat() / it.second.toFloat(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble())) }
-                .groupBy { it.first }
-                .count()
-
-        return sameLineAsteroids + topAsteroids + bottomAsteroids;
+                + countAsteroidsInSectorVisibleFromPoint(relativeAsteroidPositions.filter { it.second < 0 })
+                + countAsteroidsInSectorVisibleFromPoint(relativeAsteroidPositions.filter { it.second > 0 }))
     }
+
+    private fun countAsteroidsInSectorVisibleFromPoint(relativeAsteroidPositions: List<Pair<Int, Int>>): Int =
+            relativeAsteroidPositions
+                    .map { Pair(it.first.toDouble() / it.second.toDouble(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble())) }
+                    .groupBy { it.first }
+                    .count()
+
+    private fun getRelativeAsteroidPositions(asteroids: List<Pair<Int, Int>>, base: Pair<Int, Int>): List<Pair<Int, Int>> =
+            asteroids
+                    .map { Pair(it.first - base.first, it.second - base.second) }
+                    .filter { it.first != 0 || it.second != 0 }
+
+    private fun getAsteroidDestructionOrder(asteroids: List<Pair<Int, Int>>, base: Pair<Int, Int>): List<Pair<Int, Int>> {
+        if (asteroids.count() <= 1) return listOf()
+
+        val destroyedAsteroids = listOf(
+                getAsteroidDestructionOrderForTopRightSector(getRelativeAsteroidPositions(asteroids, base)),
+                getAsteroidDestructionOrderForHorizontalRight(getRelativeAsteroidPositions(asteroids, base)),
+                getAsteroidDestructionOrderForBottomRightSector(getRelativeAsteroidPositions(asteroids, base)),
+                getAsteroidDestructionOrderForBottomLeftSector(getRelativeAsteroidPositions(asteroids, base)),
+                getAsteroidDestructionOrderForHorizontalLeft(getRelativeAsteroidPositions(asteroids, base)),
+                getAsteroidDestructionOrderForTopLeftSector(getRelativeAsteroidPositions(asteroids, base))
+        )
+                .flatten()
+                .map { Pair(it.first + base.first, it.second + base.second) }
+
+        return (destroyedAsteroids
+                + getAsteroidDestructionOrder(
+                asteroids.filter { it !in destroyedAsteroids },
+                base
+        ))
+    }
+
+    private fun getAsteroidDestructionOrderForTopRightSector(relativeAsteroidPositions: List<Pair<Int, Int>>): List<Pair<Int, Int>> =
+            relativeAsteroidPositions
+                    .filter { it.first >= 0 && it.second < 0 }
+                    .getAsteroidDestructionOrderForSector(reverseOrder())
+
+    private fun getAsteroidDestructionOrderForHorizontalRight(relativeAsteroidPositions: List<Pair<Int, Int>>): List<Pair<Int, Int>> =
+            getAsteroidDestructionOrderOnHorizontalLine(
+                    relativeAsteroidPositions
+                            .filter { it.first > 0 && it.second == 0 }
+            )
+
+    private fun getAsteroidDestructionOrderForBottomRightSector(relativeAsteroidPositions: List<Pair<Int, Int>>): List<Pair<Int, Int>> =
+            relativeAsteroidPositions
+                    .filter { it.first >= 0 && it.second > 0 }
+                    .getAsteroidDestructionOrderForSector(naturalOrder())
+
+    private fun getAsteroidDestructionOrderForBottomLeftSector(relativeAsteroidPositions: List<Pair<Int, Int>>): List<Pair<Int, Int>> =
+            relativeAsteroidPositions
+                    .filter { it.first < 0 && it.second > 0 }
+                    .getAsteroidDestructionOrderForSector(naturalOrder())
+
+    private fun getAsteroidDestructionOrderForHorizontalLeft(relativeAsteroidPositions: List<Pair<Int, Int>>): List<Pair<Int, Int>> =
+            getAsteroidDestructionOrderOnHorizontalLine(
+                    relativeAsteroidPositions
+                            .filter { it.first < 0 && it.second == 0 }
+            )
+
+    private fun getAsteroidDestructionOrderForTopLeftSector(relativeAsteroidPositions: List<Pair<Int, Int>>): List<Pair<Int, Int>> =
+            relativeAsteroidPositions
+                    .filter { it.first < 0 && it.second < 0 }
+                    .getAsteroidDestructionOrderForSector(reverseOrder())
+
+    private fun getAsteroidDestructionOrderOnHorizontalLine(asteroidsOnHorizontalLine: List<Pair<Int, Int>>): List<Pair<Int, Int>> =
+            listOf(asteroidsOnHorizontalLine.minBy { abs(it.first) })
+                    .filterNotNull()
+
+    private fun List<Pair<Int, Int>>.getAsteroidDestructionOrderForSector(comparator: Comparator<Double>): List<Pair<Int, Int>> =
+            this
+                    .map { Pair(it, Pair(it.first.toDouble() / it.second.toDouble(), sqrt(((it.first * it.first) + (it.second * it.second)).toDouble()))) }
+                    .groupBy { it.second.first }
+                    .toSortedMap(comparator)
+                    .map { (_, info) -> info.minBy { it.second.second }!! }
+                    .map { it.first }
 }
