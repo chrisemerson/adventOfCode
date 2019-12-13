767b61aea3e093c9df88fd9a3aa2e5dc826f89ed
Chris Emerson <chris@cemerson.co.uk>
2019-12-13 05:37:19 +0000

    Day 13 both parts

---
+++ 2019-Kotlin/src/uk/co/cemerson/aoc/Day13/ArcadeMachine.kt
@@ -0,0 +1,79 @@
+package uk.co.cemerson.aoc.Day13
+
+import uk.co.cemerson.aoc.Util.IntCode.InputProvider
+import uk.co.cemerson.aoc.Util.IntCode.OutputConsumer
+import java.math.BigInteger
+
+class ArcadeMachine : InputProvider, OutputConsumer {
+    private var outputCount = 0
+    private var grid = mutableListOf<Triple<BigInteger, BigInteger, Int>>()
+
+    private var x = 0.toBigInteger()
+    private var y = 0.toBigInteger()
+
+    private var ballx = 0.toBigInteger()
+    private var paddlex = 0.toBigInteger()
+
+    private var score = 0
+
+    private var inputList = mutableListOf<BigInteger>()
+
+    override fun getInput(): BigInteger? {
+        var move = 0
+
+        if (paddlex < ballx) {
+            move = 1
+        } else if (paddlex > ballx) {
+            move = -1
+        }
+
+        return move.toBigInteger()
+    }
+
+    override fun consumeOutput(output: BigInteger) {
+        when (outputCount) {
+            0 -> collectXCoord(output)
+            1 -> collectYCoord(output)
+            2 -> collectTileType(output)
+        }
+
+        outputCount = (outputCount + 1) % 3
+    }
+
+    private fun collectXCoord(output: BigInteger) {
+        x = output
+    }
+
+    private fun collectYCoord(output: BigInteger) {
+        y = output
+    }
+
+    private fun collectTileType(output: BigInteger) {
+        if (x == -1.toBigInteger() && y == 0.toBigInteger()) {
+            score = output.toInt()
+        } else {
+            grid.add(Triple(x, y, output.toInt()))
+
+            if (output.toInt() == 3) {
+                paddlex = x
+            }
+
+            if (output.toInt() == 4) {
+                ballx = x
+            }
+        }
+    }
+
+    fun getBlockTilesLeft(): Int = grid.filter { it.third == 2 }.count()
+
+    fun getScore(): Int = score
+
+    fun feedInput(input: List<BigInteger>) {
+        inputList = input.toMutableList()
+    }
+
+    fun gameEnded(): Boolean = grid.count() != 0 && getBlockTilesLeft() == 0
+
+    override fun consumeFinalValueInPositionZero(output: BigInteger) {
+    }
+}
--- 2019-Kotlin/src/uk/co/cemerson/aoc/Day13/Day13.kt
+++ 2019-Kotlin/src/uk/co/cemerson/aoc/Day13/Day13.kt
@@ -1,13 +1,33 @@
 package uk.co.cemerson.aoc.Day13
 
 import uk.co.cemerson.aoc.AOCDay
+import uk.co.cemerson.aoc.Util.IntCode.Computer
+import java.math.BigInteger
 
 class Day13 : AOCDay {
     override fun part1() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val arcadeMachine = ArcadeMachine()
+        val computer = Computer(arcadeMachine, arcadeMachine)
+        val program = getProgram()
+
+        computer.execute(program)
+
+        println(arcadeMachine.getBlockTilesLeft())
     }
 
     override fun part2() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val arcadeMachine = ArcadeMachine()
+        val computer = Computer(arcadeMachine, arcadeMachine)
+        val program = getProgram().toMutableList()
+
+        program[0] = 2.toBigInteger()
+
+        computer.execute(program)
+
+        println(arcadeMachine.getScore())
     }
+
+    private fun getProgram(): List<BigInteger> =
+            readFileSplitByChar(filename = "Day13/input.txt", splitBy = ',')
+                    .map { it.toBigInteger() }
 }
