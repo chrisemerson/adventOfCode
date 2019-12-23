4f17434ace18d833a1c20841a28e1cd72a329c89
Chris Emerson <chris@cemerson.co.uk>
2019-12-23 00:01:25 +0000

    Day 22 both parts. Convert processing into linear functions that can be combined into a single linear function to find any card's new position. Required reversing with the modInverse() to get the card from the position

--- 2019-Kotlin/src/uk/co/cemerson/aoc/Day22/Day22.kt
+++ 2019-Kotlin/src/uk/co/cemerson/aoc/Day22/Day22.kt
@@ -1,64 +1,84 @@
 package uk.co.cemerson.aoc.Day22
 
 import uk.co.cemerson.aoc.AOCDay
-import java.lang.Math.abs
+import java.math.BigInteger
+
 
 class Day22 : AOCDay {
     override fun part1() {
-        val instructions = getInput()
-        val shuffledCards = processInstructions(instructions, (0..10006).toList())
+        val deckSize = 10007.toBigInteger()
+        val totalCoeffs = getCoefficientsOfLinearTransformForSingleShuffle(deckSize)
 
-        println(shuffledCards.withIndex().filter { it.value == 2019 }.first().index)
+        println("Index of card 2019: " + applyModularLinearTransformToSingleNumber(totalCoeffs.first, totalCoeffs.second, deckSize, 2019.toBigInteger()))
     }
 
     override fun part2() {
+        val deckSize = "119315717514047".toBigInteger()
+        val shuffles = "101741582076661".toBigInteger()
+        val coeffsForSingleShuffle = getCoefficientsOfLinearTransformForSingleShuffle(deckSize)
 
+        val coeffsForPowersOfTwo = mutableMapOf(0 to coeffsForSingleShuffle)
 
+        for (i in 1..50) {
+            val coeffsForPowerOfTwoBeforeThisOne = coeffsForPowersOfTwo[i - 1]!!
+            val coeffsForThisPowerOfTwo = combineLinearFunctions(listOf(coeffsForPowerOfTwoBeforeThisOne, coeffsForPowerOfTwoBeforeThisOne))
+
+            coeffsForPowersOfTwo[i] = Pair(coeffsForThisPowerOfTwo.first % deckSize, coeffsForThisPowerOfTwo.second % deckSize)
+        }
+
+        val coeffsForThisShuffle = mutableListOf<Pair<BigInteger, BigInteger>>()
+
+        for (i in 0..50) {
+            val bit = 2.toBigInteger().pow(i)
+
+            if (shuffles and bit != 0.toBigInteger()) {
+                coeffsForThisShuffle.add(coeffsForPowersOfTwo[i]!!)
+            }
+        }
+
+        val totalCoeffs = combineLinearFunctions(coeffsForThisShuffle)
+        val moduloInverse = totalCoeffs.first.modInverse(deckSize)
+        val cardInPosition2020 = ((((2020.toBigInteger() + deckSize - totalCoeffs.second) * moduloInverse) % deckSize) + deckSize) % deckSize
+
+        println("Card in position 2020: " + cardInPosition2020)
+        println("Position of card " + cardInPosition2020 + ": " + applyModularLinearTransformToSingleNumber(totalCoeffs.first, totalCoeffs.second, deckSize, cardInPosition2020))
     }
 
-    private fun processInstructions(instructions: List<String>, initialDeck: List<Int>): List<Int> =
-            instructions.fold(initialDeck) { deck, instruction -> processInstruction(instruction, deck) }
+    private fun getCoefficientsOfLinearTransformForSingleShuffle(deckSize: BigInteger): Pair<BigInteger, BigInteger> {
+        val instructions = getInput()
+        val linearCoeffs = convertInstructionsToCoefficientsForLinearEquation(instructions)
+        val totalCoeffs = combineLinearFunctions(linearCoeffs)
 
-    private fun processInstruction(instruction: String, deck: List<Int>): List<Int> {
+        return Pair(totalCoeffs.first % deckSize, totalCoeffs.second % deckSize)
+    }
+
+    private fun convertInstructionsToCoefficientsForLinearEquation(instructions: List<String>): List<Pair<BigInteger, BigInteger>> =
+            instructions.map { convertSingleInstructionToCoefficientsForLinearEquation(it) }
+
+    private fun convertSingleInstructionToCoefficientsForLinearEquation(instruction: String): Pair<BigInteger, BigInteger> {
         if (instruction == "deal into new stack") {
-            return dealIntoNewStack(deck)
+            return Pair(-1.toBigInteger(), -1.toBigInteger())
         }
 
         val cutMatchResult = "cut (-?\\d+)".toRegex().matchEntire(instruction)
         if (cutMatchResult != null) {
-            return cutCards(deck, cutMatchResult.groups[1]!!.value.toInt())
+            return Pair(1.toBigInteger(), 0.toBigInteger() - cutMatchResult.groups[1]!!.value.toBigInteger())
         }
 
         val dealIncrementMatchResult = "deal with increment (\\d+)".toRegex().matchEntire(instruction)
         if (dealIncrementMatchResult != null) {
-            return dealWithIncrement(deck, dealIncrementMatchResult.groups[1]!!.value.toInt())
+            return Pair(dealIncrementMatchResult.groups[1]!!.value.toBigInteger(), 0.toBigInteger())
         }
 
-        return deck
+        return Pair(1.toBigInteger(), 0.toBigInteger())
     }
 
-    private fun dealIntoNewStack(deck: List<Int>): List<Int> {
-        println("Dealing into new stack")
-        return deck.reversed()
-    }
+    private fun combineLinearFunctions(functions: List<Pair<BigInteger, BigInteger>>): Pair<BigInteger, BigInteger> =
+            functions
+                    .reduce { a: Pair<BigInteger, BigInteger>, b: Pair<BigInteger, BigInteger> -> Pair(a.first * b.first, b.first * a.second + b.second) }
 
-    private fun cutCards(deck: List<Int>, cutAmount: Int): List<Int> {
-        println("Cutting " + cutAmount + " cards")
-
-        return if (cutAmount < 0) deck.takeLast(abs(cutAmount)) + deck.take(deck.count() + cutAmount)
-        else deck.drop(cutAmount) + deck.take(cutAmount)
-    }
-
-    private fun dealWithIncrement(deck: List<Int>, increment: Int): List<Int> {
-        println("Deal with increment " + increment)
-        val outputDeck = mutableMapOf<Int, Int>()
-
-        for (i in 0.until(deck.count())) {
-            outputDeck[(i * increment) % deck.count()] = deck[i]
-        }
-
-        return outputDeck.toSortedMap().toList().map { it.second }
-    }
+    private fun applyModularLinearTransformToSingleNumber(a: BigInteger, b: BigInteger, n: BigInteger, x: BigInteger): BigInteger =
+            (((a * x + b) % n) + n) % n
 
     private fun getInput(): List<String> =
             readFileLines("Day22/input.txt")
