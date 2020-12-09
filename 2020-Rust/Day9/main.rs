533e0e44f752abb8c3bbf0361d911028cd2a68c3
Chris Emerson <chris@cemerson.co.uk>
2020-12-09 18:07:04 +0000

    Day 9 parts 1 and 2

---
+++ 2020-Rust/Day9/main.rs
@@ -0,0 +1,64 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt")
+        .expect("Something went wrong reading the file");
+
+    let numbers = input
+        .lines()
+        .filter(|line| line != &"")
+        .map(|number| number.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    let slice_size = 25;
+    let mut sum:i64 = 0;
+
+    for i in 0..(numbers.len() - &slice_size - 1) {
+        let prev_numbers:Vec<i64> = numbers[i..i + &slice_size].to_vec();
+        let number:i64 = numbers[i + &slice_size];
+
+        if !can_number_be_sum_of_previous_numbers(&number, &prev_numbers) {
+            sum = number;
+
+            println!("First number that cannot be the sum of previous {} numbers: {}", slice_size, number);
+            break;
+        }
+    }
+
+    for start in 0..numbers.len() {
+        for end in start..numbers.len() {
+            let contiguous_numbers:Vec<i64> = numbers[start..end + 1].to_vec();
+            let sum_of_contiguous_numbers = contiguous_numbers.iter().fold(0, |a, b| a + b);
+
+            if contiguous_numbers.len() != 1 && sum_of_contiguous_numbers == sum {
+                println!("Contiguous group found that sum to {}: {:?}", &sum, contiguous_numbers);
+
+                let min_number = contiguous_numbers.iter().min().unwrap();
+                let max_number = contiguous_numbers.iter().max().unwrap();
+
+                println!(
+                    "Sum of start and end of group ({} and {}) is {}",
+                    min_number,
+                    max_number,
+                    min_number + max_number
+                );
+            }
+
+            if sum_of_contiguous_numbers > sum {
+                break;
+            }
+        }
+    }
+}
+
+fn can_number_be_sum_of_previous_numbers(number: &i64, prev_numbers: &Vec<i64>) -> bool {
+    for i in 0..prev_numbers.len() {
+        for j in 0..prev_numbers.len() {
+            if i != j && prev_numbers[i] + prev_numbers[j] == number.to_owned() {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
