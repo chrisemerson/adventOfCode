a8e2939d5359fe963f06a8262d1dfc6c6fbf39b4
Chris Emerson <chris.emerson@bjss.com>
2021-12-01 11:56:08 +0000

    Day 1 Part 2

--- 2021-TypeScript/Day1/Day1.ts
+++ 2021-TypeScript/Day1/Day1.ts
@@ -10,17 +10,15 @@ function part1 (filename: string) {
 
 function part2 (filename: string) {
     const counter = calculateLargerMeasurements(
-        getInput(filename)
+        sumArrayPartitions(
+            partitionArrayWithSlidingWindow(
+                getInput(filename),
+                3
+            )
+        )
     );
 
-    console.log(counter + " measurements are larger than the previous measurement");
-}
-
-function getInput(filename: string) {
-    return fs
-        .readFileSync(filename, 'utf8')
-        .split("\n")
-        .map(l => parseInt(l));
+    console.log(counter + " measurements are larger than the previous measurement using a sliding window");
 }
 
 function calculateLargerMeasurements(input: number[]) {
@@ -35,6 +33,41 @@ function calculateLargerMeasurements(input: number[]) {
     }
 
     return counter;
+}
+
+function partitionArrayWithSlidingWindow(array: number[], partitionSize: number): number[][] {
+    let partitionedArray: number[][] = [];
+
+    for (let i = 0; i + partitionSize <= array.length; i++) {
+        let thisPartition = [];
+
+        for (let j = 0; j < partitionSize; j++) {
+            thisPartition.push(array[i + j]);
+        }
+
+        partitionedArray.push(thisPartition);
+    }
+
+    return partitionedArray;
+}
+
+function sumArrayPartitions(array: number[][]): number[] {
+    let arrayOfSums: number[] = [];
+
+    for (let window of array) {
+        arrayOfSums.push(
+            window.reduce((sum, current) => sum + current, 0)
+        );
+    }
+
+    return arrayOfSums;
+}
+
+function getInput(filename: string) {
+    return fs
+        .readFileSync(filename, 'utf8')
+        .split("\n")
+        .map(l => parseInt(l));
 }
 
 export { part1, part2 };
