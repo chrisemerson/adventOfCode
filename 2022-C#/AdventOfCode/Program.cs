d6147d940abde0d815f62593a754118e3497bf91
Chris Emerson <chris@cemerson.co.uk>
2022-11-30 20:41:17 +0000

    Add execution 'harness' for AoC that will run any given day and part with input

--- 2022-C#/AdventOfCode/Program.cs
+++ 2022-C#/AdventOfCode/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AdventOfCode;
 
-Console.WriteLine("Hello, World!");
+if (args.Length >= 2) {
+    var dayString = args[0];
+    var partString = args[1];
+
+    var day = Type.GetType("AdventOfCode.Day" + dayString);
+
+    if (day != null && Activator.CreateInstance(day) is IAdventOfCodeDay dayInstance) {
+        var input = "";
+
+        if (args.Length == 3) {
+            input = args[2];
+        } else {
+            var inputFile = Directory.GetCurrentDirectory() + "/Day" + dayString + "/input.txt";
+
+            if (File.Exists(inputFile)) {
+                input = File.ReadAllText(inputFile);
+            } else {
+                Console.Error.WriteLine("Input not provided and input file not found");
+                Environment.Exit(1);
+            }
+        }
+
+        switch (partString) {
+            case "1":
+                dayInstance.Part1(input);
+                break;
+
+            case "2":
+                dayInstance.Part2(input);
+                break;
+
+            default:
+                Console.Error.WriteLine($"Invalid Part no: {partString}");
+                break;
+        }
+    } else {
+        Console.Error.WriteLine($"Invalid Day no: {dayString}");
+    }
+} else {
+    Console.Error.WriteLine("Day and Part number required");
+}
