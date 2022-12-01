605ecbc2a2fabc4f5400386e0d1ad36e46191294
Chris Emerson <chris.emerson@bjss.com>
2022-12-01 09:46:53 +0000

    Turn methods into arrow methods

--- 2022-C#/AdventOfCode/Day1/Day1.cs
+++ 2022-C#/AdventOfCode/Day1/Day1.cs
@@ -2,24 +2,17 @@
 
 public class Day1: IAdventOfCodeDay
 {
-    public void Part1(string input)
-    {
-        Console.WriteLine("Max calories of any elf is : " + getCaloriesByElf(input).Max());
-    }
+    public void Part1(string input) =>
+        Console.WriteLine("Max calories of any elf is: " + getCaloriesByElf(input).Max());
 
-    public void Part2(string input)
-    {
+    public void Part2(string input) =>
         Console.WriteLine("Max calories of top 3 elves is: " + getCaloriesByElf(input).OrderDescending().Take(3).Sum());
-    }
 
-    private List<int> getCaloriesByElf(string input)
-    {
-        return input.Split("\n\n")
-            .Select(elf => elf
-                .Split("\n")
-                .Where(calorie => calorie != "")
-                .Select(calorie => int.Parse(calorie))
-                .Sum())
-            .ToList();
-    }
+    private List<int> getCaloriesByElf(string input) => input.Split("\n\n")
+        .Select(elf => elf
+            .Split("\n")
+            .Where(calorie => calorie != "")
+            .Select(calorie => int.Parse(calorie))
+            .Sum())
+        .ToList();
 }
