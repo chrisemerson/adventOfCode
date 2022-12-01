8b6882a31e49d8b225055510d0e965758e0238c6
Chris Emerson <chris.emerson@bjss.com>
2022-12-01 09:28:48 +0000

    Day 1 both parts

--- 2022-C#/AdventOfCode/Day1/Day1.cs
+++ 2022-C#/AdventOfCode/Day1/Day1.cs
@@ -4,11 +4,21 @@ public class Day1: IAdventOfCodeDay
 {
     public void Part1(string input)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Max calories of any elf is : " + getCaloriesByElf(input).Max());
     }
 
     public void Part2(string input)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Max calories of top 3 elves is: " + getCaloriesByElf(input).OrderDescending().Take(3).Sum());
+    }
+
+    private List<int> getCaloriesByElf(string input)
+    {
+        return input.Split("\n\n")
+            .Select(elf => elf
+                .Split("\n")
+                .Where(calorie => calorie != "")
+                .Select(calorie => int.Parse(calorie)).Sum())
+            .ToList();
     }
 }
