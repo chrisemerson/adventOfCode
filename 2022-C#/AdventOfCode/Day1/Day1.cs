e6945fb9b66f2b7763d699f1b18b61b9d3d0508c
Chris Emerson <chris.emerson@bjss.com>
2022-12-01 09:48:58 +0000

    Some more small refactoring

--- 2022-C#/AdventOfCode/Day1/Day1.cs
+++ 2022-C#/AdventOfCode/Day1/Day1.cs
@@ -3,16 +3,16 @@
 public class Day1: IAdventOfCodeDay
 {
     public void Part1(string input) =>
-        Console.WriteLine("Max calories of any elf is: " + getCaloriesByElf(input).Max());
+        Console.WriteLine("Max calories of any elf is: " + GetCaloriesByElf(input).Max());
 
     public void Part2(string input) =>
-        Console.WriteLine("Max calories of top 3 elves is: " + getCaloriesByElf(input).OrderDescending().Take(3).Sum());
+        Console.WriteLine("Max calories of top 3 elves is: " + GetCaloriesByElf(input).OrderDescending().Take(3).Sum());
 
-    private List<int> getCaloriesByElf(string input) => input.Split("\n\n")
+    private static IEnumerable<int> GetCaloriesByElf(string input) => input.Split("\n\n")
         .Select(elf => elf
             .Split("\n")
             .Where(calorie => calorie != "")
-            .Select(calorie => int.Parse(calorie))
+            .Select(int.Parse)
             .Sum())
         .ToList();
 }
