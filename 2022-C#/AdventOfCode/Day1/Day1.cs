053e0cd6950f6134a195e5622d8edececbea0836
Chris Emerson <chris.emerson@bjss.com>
2022-12-01 13:17:47 +0000

    Correct wording slightly

--- 2022-C#/AdventOfCode/Day1/Day1.cs
+++ 2022-C#/AdventOfCode/Day1/Day1.cs
@@ -6,7 +6,7 @@ public class Day1: IAdventOfCodeDay
         Console.WriteLine("Max calories of any elf is: " + GetCaloriesByElf(input).Max());
 
     public void Part2(string input) =>
-        Console.WriteLine("Max calories of top 3 elves is: " + GetCaloriesByElf(input).OrderDescending().Take(3).Sum());
+        Console.WriteLine("Sum of calories of top 3 elves is: " + GetCaloriesByElf(input).OrderDescending().Take(3).Sum());
 
     private static IEnumerable<int> GetCaloriesByElf(string input) => input.Split("\n\n")
         .Select(elf => elf
