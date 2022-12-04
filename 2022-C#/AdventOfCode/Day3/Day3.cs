ddb21bf3b3dbff164745b42b3401bea70f0e8af0
Chris Emerson <chris@cemerson.co.uk>
2022-12-04 14:22:29 +0000

    Day 3 both parts

--- 2022-C#/AdventOfCode/Day3/Day3.cs
+++ 2022-C#/AdventOfCode/Day3/Day3.cs
@@ -1,14 +1,27 @@
 namespace AdventOfCode;
 
-public class Day3: IAdventOfCodeDay
+public class Day3 : IAdventOfCodeDay
 {
-    public void Part1(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public void Part1(string input) => Console.WriteLine(input
+        .Split("\n")
+        .Where(l => l != "")
+        .Select(s => s[..(s.Length / 2)]
+            .ToHashSet()
+            .Intersect(s[(s.Length / 2)..])
+            .ElementAt(0))
+        .Select(Priority)
+        .Sum());
 
-    public void Part2(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public void Part2(string input) => Console.WriteLine(input
+        .Split("\n")
+        .Where(l => l != "")
+        .Chunk(3)
+        .Select(g => g[0].ToHashSet()
+            .Intersect(g[1])
+            .Intersect(g[2])
+            .ElementAt(0))
+        .Select(Priority)
+        .Sum());
+
+    private static int Priority(char c) => c > 96 ? c - 96 : c - 38;
 }
