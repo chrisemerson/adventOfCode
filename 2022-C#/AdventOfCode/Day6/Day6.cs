64cf9e9a4b18e45eec4187f407d6d40c92ae25d8
Chris Emerson <chris.emerson@bjss.com>
2022-12-06 09:49:49 +0000

    Day 6 both parts

--- 2022-C#/AdventOfCode/Day6/Day6.cs
+++ 2022-C#/AdventOfCode/Day6/Day6.cs
@@ -2,13 +2,18 @@ namespace AdventOfCode;
 
 public class Day6: IAdventOfCodeDay
 {
-    public void Part1(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public void Part1(string input) => FindFirstSetOfNDifferentCharacters(input, 4);
+    public void Part2(string input) => FindFirstSetOfNDifferentCharacters(input, 14);
 
-    public void Part2(string input)
+    private static void FindFirstSetOfNDifferentCharacters(string input, int count)
     {
-        throw new NotImplementedException();
+        for (var i = count - 1; i < input.Length; i++)
+        {
+            if (input.Substring(i - count + 1, count).ToHashSet().Count == count)
+            {
+                Console.WriteLine("First marker after " + (i + 1) + " characters");
+                break;
+            }
+        }
     }
 }
