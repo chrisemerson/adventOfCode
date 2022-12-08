441c2d5bb084569d2f1dc2caa740f258496cbdcb
Chris Emerson <chris.emerson@bjss.com>
2022-12-08 14:23:08 +0000

    Another refactor

--- 2022-C#/AdventOfCode/Day8/Day8.cs
+++ 2022-C#/AdventOfCode/Day8/Day8.cs
@@ -2,19 +2,22 @@ namespace AdventOfCode;
 
 public class Day8 : IAdventOfCodeDay
 {
-    public void Part1(string input) => Console.WriteLine(
-        "Visible trees: " + BuildTreeGridFromInput(input)
+    public void Part1(string input) => FindNumberOfVisibleTreesFromEdge(BuildTreeGridFromInput(input));
+    public void Part2(string input) => FindTreeWithBestScenicScore(BuildTreeGridFromInput(input));
+
+    private static void FindNumberOfVisibleTreesFromEdge(int[][] treeGrid) => Console.WriteLine(
+        "Visible trees: " + treeGrid
             .Select((r, y) => r
-                .Select((_, x) => IsVisibleFromEdge(x, y, BuildTreeGridFromInput(input)))
+                .Select((_, x) => IsVisibleFromEdge(x, y, treeGrid))
                 .Count(t => t))
             .Sum());
 
-    public void Part2(string input) => Console.WriteLine(
-        "Best scenic score: " + BuildTreeGridFromInput(input)
+    private static void FindTreeWithBestScenicScore(int[][] treeGrid) => Console.WriteLine(
+        "Best scenic score: " + treeGrid
             .Select((r, y) => r
-                .Select((_, x) => GetScenicScoreForTree(x, y, BuildTreeGridFromInput(input)))
-                .Max()
-            ).Max());
+                .Select((_, x) => GetScenicScoreForTree(x, y, treeGrid))
+                .Max())
+            .Max());
 
     private static int[][] BuildTreeGridFromInput(string input) => input
         .Split("\n")
@@ -24,20 +27,20 @@ public class Day8 : IAdventOfCodeDay
             .ToArray())
         .ToArray();
 
-    private static bool IsVisibleFromEdge(int x, int y, IReadOnlyList<int[]> grid) => GetSightLinesFromTree(x, y, grid)
+    private static bool IsVisibleFromEdge(int x, int y, int[][] grid) => GetSightLinesFromTree(x, y, grid)
         .Select(r => r.Count(c => grid[c.Item2][c.Item1] >= grid[y][x]))
         .Count(c => c > 0) < 4;
 
-    private static int GetScenicScoreForTree(int x, int y, IReadOnlyList<int[]> grid) =>
+    private static int GetScenicScoreForTree(int x, int y, int[][] grid) =>
         GetSightLinesFromTree(x, y, grid)
             .Select(r => r.Select(t => grid[t.Item2][t.Item1]))
             .Select(r => Math.Min(r.Count(), r.TakeWhile(t => t < grid[y][x]).Count() + 1))
             .Aggregate(1, (a, b) => a * b);
 
-    private static IEnumerable<(int, int)>[] GetSightLinesFromTree(int x, int y, IReadOnlyList<int[]> grid) => new[] {
+    private static IEnumerable<(int, int)>[] GetSightLinesFromTree(int x, int y, int[][] grid) => new[] {
         Enumerable.Range(0, y).Select(dy => (x, dy)).Reverse(),
-        Enumerable.Range(y + 1, grid.Count - y - 1).Select(dy => (x, dy)),
+        Enumerable.Range(y + 1, grid.Length - y - 1).Select(dy => (x, dy)),
         Enumerable.Range(0, x).Select(dx => (dx, y)).Reverse(),
-        Enumerable.Range(x + 1, grid.Count - x - 1).Select(dx => (dx, y))
+        Enumerable.Range(x + 1, grid.Length - x - 1).Select(dx => (dx, y))
     };
 }
