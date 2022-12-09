6191eddbacf1f9006b6a951cb3987d81a81859a2
Chris Emerson <chris.emerson@bjss.com>
2022-12-09 16:11:12 +0000

    Tidy up

--- 2022-C#/AdventOfCode/Day9/Day9.cs
+++ 2022-C#/AdventOfCode/Day9/Day9.cs
@@ -17,9 +17,9 @@ public class Day9 : IAdventOfCodeDay
             .Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
             .Select(l => l.Split(" "))
             .Aggregate(
-                ((0, 0), ((0, 0), new HashSet<(int, int)>() { (0, 0) }.ToImmutableHashSet())),
+                ((0, 0), (0, 0), new HashSet<(int, int)> { (0, 0) }.ToImmutableHashSet()),
                 (acc, x) => MakeMove(acc, x.ElementAt(0)[0], int.Parse(x.ElementAt(1))))
-            .Item2.Item2
+            .Item3
             .Count
         + " positions");
 
@@ -30,32 +30,29 @@ public class Day9 : IAdventOfCodeDay
         throw new NotImplementedException();
     }
 
-    private static ((int, int), ((int, int), ImmutableHashSet<(int, int)>)) MakeMove(
-        ((int, int), ((int, int), ImmutableHashSet<(int, int)>)) ropeState,
+    private static ((int, int), (int, int), ImmutableHashSet<(int, int)>) MakeMove(
+        ((int, int), (int, int), ImmutableHashSet<(int, int)>) ropeState,
         char dir,
         int dist
-    ) => (
-        UpdateHeadPosition(ropeState.Item1, dir, dist),
-        UpdateTailPosition(UpdateHeadPosition(ropeState.Item1, dir, dist), ropeState.Item2.Item1,
-            ropeState.Item2.Item2));
+    ) => UpdateTailPositionAndVisitedCells(GetNextHeadPosition(ropeState.Item1, dir, dist), ropeState.Item2, ropeState.Item3);
 
-    private static (int, int) UpdateHeadPosition((int, int) headPos, char dir, int dist) => (
+    private static (int, int) GetNextHeadPosition((int, int) headPos, char dir, int dist) => (
         headPos.Item1 + dist * MoveMap[dir].Item1,
         headPos.Item2 + dist * MoveMap[dir].Item2
     );
 
-    private static ((int, int), ImmutableHashSet<(int, int)>) UpdateTailPosition(
+    private static ((int, int), (int, int), ImmutableHashSet<(int, int)>) UpdateTailPositionAndVisitedCells(
         (int, int) headPos,
         (int, int) tailPos,
         ImmutableHashSet<(int, int)> visited
     ) => Math.Abs(headPos.Item1 - tailPos.Item1) <= 1 && Math.Abs(headPos.Item2 - tailPos.Item2) <= 1
-        ? (tailPos, visited)
-        : UpdateTailPosition(
+        ? (headPos, tailPos, visited)
+        : UpdateTailPositionAndVisitedCells(
             headPos,
-            GetTailMovement(headPos, tailPos),
-            visited.Add(GetTailMovement(headPos, tailPos)));
+            GetNextTailPosition(headPos, tailPos),
+            visited.Add(GetNextTailPosition(headPos, tailPos)));
 
-    private static (int, int) GetTailMovement((int, int) headPos, (int, int) tailPos) => (
+    private static (int, int) GetNextTailPosition((int, int) headPos, (int, int) tailPos) => (
         tailPos.Item1 == headPos.Item1
             ? tailPos.Item1
             : tailPos.Item1 + (headPos.Item1 - tailPos.Item1) / Math.Abs(headPos.Item1 - tailPos.Item1),
