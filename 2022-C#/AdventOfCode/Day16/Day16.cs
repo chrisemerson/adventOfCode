48cb0c622d0d54166cf38b890a7d159abec621d2
Chris Emerson <chris@cemerson.co.uk>
2022-12-19 00:12:10 +0000

    Input parsing

--- 2022-C#/AdventOfCode/Day16/Day16.cs
+++ 2022-C#/AdventOfCode/Day16/Day16.cs
@@ -1,14 +1,33 @@
 namespace AdventOfCode;
 
-public class Day16: IAdventOfCodeDay
+public class Day16 : IAdventOfCodeDay
 {
     public void Part1(string input)
     {
-        throw new NotImplementedException();
+        var valves = ParseInput(input);
     }
 
     public void Part2(string input)
     {
         throw new NotImplementedException();
+    }
+
+    private static IEnumerable<(string, int, string[])> ParseInput(string input)
+    {
+        return input
+            .Split("\n", StringSplitOptions.RemoveEmptyEntries)
+            .Select(l =>
+            {
+                var valveParts = l.Split("; ");
+                var valveInfo = valveParts[0].Split(" has flow rate=");
+
+                return (
+                    valveInfo[0].Substring(6),
+                    int.Parse(valveInfo[1]),
+                    valveParts[1].StartsWith("tunnel ")
+                        ? valveParts[1][22..].Split(",", StringSplitOptions.TrimEntries)
+                        : valveParts[1][23..].Split(",", StringSplitOptions.TrimEntries)
+                );
+            });
     }
 }
