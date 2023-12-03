edb70f59e7e7843a9d728520c8e915677367a6c4
Chris Emerson <git@chris.emerson.email>
2023-12-03 21:23:56 +0000

    Day 3 Part 2

--- 2023-Haskell/app/Day3/Day3.hs
+++ 2023-Haskell/app/Day3/Day3.hs
@@ -1,31 +1,44 @@
 module Day3.Day3 where
     import Data.Char
     import Data.Typeable
-    import Data.Vector (fromList, ifoldl, imap, ifilter, Vector, (!), toList)
+    import Data.Vector (fromList, ifoldl, imap, ifilter, Vector, (!), toList, map)
     import Util
+
+    vmap = Data.Vector.map
+    pmap = Prelude.map
 
     data Number = Number {row :: Int, start :: Int, end :: Int, number :: Int} deriving (Show)
     data ParseState = ParseState {numbers :: [Number], currentlyInDigit :: Bool, startPos :: Int, curNumber :: Int} deriving (Show)
+    data Gear = Gear {y :: Int, x :: Int} deriving (Show)
 
     part1 :: String -> String
     part2 :: String -> String
 
     part1 input = sumEnginePartNumbers input
-    part2 input = sumEnginePartNumbers input
+    part2 input = show $ sumGearRatios input
 
     sumEnginePartNumbers input =
-        show $ sum (map (\x -> number x) (filter (\x -> hasAdjacentSymbol x inputGrid) (findNumbers inputGrid))) where
+        show $ sum (pmap number (filter (hasAdjacentSymbol inputGrid) (findNumbers inputGrid))) where
             inputGrid = getInputGrid input
 
+    sumGearRatios input = sum (pmap product (filter (\x -> length x == 2) numbersAdjacentToGears)) where
+        numbersAdjacentToGears = pmap (pmap number . (findNumbersAdjacentToGear numbers)) (findGears inputGrid)
+        numbers = findNumbers inputGrid
+        inputGrid = getInputGrid input
+
     getInputGrid :: String -> Vector (Vector Char)
-    getInputGrid input = fromList (map (fromList . trim) (lines input))
+    getInputGrid input = fromList (pmap (fromList . trim) (lines input))
 
     findNumbers inputVec = concat $ imap findNumbersInRow inputVec
+
+    findGears inputVec = concat $ imap findGearsInRow inputVec
 
     findNumbersInRow rowNo lineVec = numbers (ifoldl
         (\a i c -> updateParseState a c rowNo i)
         ParseState {numbers = [], currentlyInDigit = False, startPos = 0, curNumber = 0}
         lineVec)
+
+    findGearsInRow rowNo lineVec = ifoldl (\a i c -> if c == '*' then a ++ [Gear {y = rowNo, x = i}] else a) [] lineVec
 
     updateParseState parseState nextChar rowNo pos = if isDigit nextChar
         then if currentlyInDigit parseState
@@ -55,12 +68,11 @@ module Day3.Day3 where
             }
             else parseState
 
-    hasAdjacentSymbol :: Number -> Vector (Vector Char) -> Bool
-    hasAdjacentSymbol number inputGrid = hasSymbol adjacentCells where
+    hasAdjacentSymbol inputGrid number = hasSymbol adjacentCells where
         adjacentCells = selectAdjacentCells (row number) (start number) (end number) inputGrid
 
     hasSymbol :: Vector (Vector Char) -> Bool
-    hasSymbol cells = or (imap (\i x -> hasSymbolInRow x) cells)
+    hasSymbol cells = or (vmap hasSymbolInRow cells)
 
     hasSymbolInRow :: Vector Char -> Bool
     hasSymbolInRow cells = or (imap (\i x -> not (or [isDigit x, x == '.'])) cells)
@@ -71,3 +83,10 @@ module Day3.Day3 where
 
     selectAdjacentColumns :: Int -> Int -> Vector Char -> Vector Char
     selectAdjacentColumns start end row = ifilter (\x y -> and [(x >= (start - 1)), (x <= (end + 1))]) row
+
+    findNumbersAdjacentToGear numbers gear = filter (numberIsAdjacentToGear gear) numbers
+
+    numberIsAdjacentToGear gear number = and [
+        abs ((y gear) - (row number)) <= 1,
+        (x gear) >= ((start number) - 1),
+        (x gear) <= ((end number) + 1)]
