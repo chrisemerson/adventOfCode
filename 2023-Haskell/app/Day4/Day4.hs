efa5df28c0e14a286251d96704c63c43053befa3
Chris Emerson <git@chris.emerson.email>
2023-12-04 10:20:29 +0000

    Day 4 Part 1

--- 2023-Haskell/app/Day4/Day4.hs
+++ 2023-Haskell/app/Day4/Day4.hs
@@ -1,6 +1,26 @@
 module Day4.Day4 where
-  part1 :: String -> String
-  part2 :: String -> String
+    import Data.List
+    import Data.List.Split
+    import Util
 
-  part1 input = input
-  part2 input = input
+    part1 :: String -> String
+    part2 :: String -> String
+
+    data Card = Card{ cardNo :: Int, winningNumbers :: [Int], ourNumbers :: [Int] } deriving (Show)
+
+    part1 input = show $ getAllScratchcardPoints (parseCards input)
+    part2 input = input
+
+    parseCards input = map parseCard (lines input)
+
+    parseCard line = Card {
+        cardNo = read (trim (take 3 (drop 5 line))),
+        winningNumbers = map read (filter (\x -> x /= "") (map (trim) (splitOn " " (take 31 (drop 9 line))))),
+        ourNumbers = map read (filter (\x -> x /= "") (map (trim) (splitOn " " (drop 42 line))))
+    }
+
+    getAllScratchcardPoints scratchcards = sum (map getScratchcardPoints scratchcards)
+
+    getScratchcardPoints scratchcard = if matchingNumbers == 0 then 0 else 2 ^ (matchingNumbers - 1) where
+        matchingNumbers = length (intersect (winningNumbers scratchcard) (ourNumbers scratchcard))
+
