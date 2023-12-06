f98865c7e55123822c2eadc8ee4b587cf45cf81a
Chris Emerson <git@chris.emerson.email>
2023-12-06 00:09:58 +0000

    Day 5 part 2

--- 2023-Haskell/app/Day5/Day5.hs
+++ 2023-Haskell/app/Day5/Day5.hs
@@ -7,13 +7,17 @@ module Day5.Day5 where
     part2 :: String -> String
 
     part1 input = show $ findLowestLocation (parseInput input False)
-    part2 input = show $ findLowestLocation (parseInput input True)
+    part2 input = show $ findSeedForLowestLocation (parseInput input True)
 
     findLowestLocation :: Almanac -> Int
     findLowestLocation almanac = minimum (map (\x -> minimum (map (findLocationForSeed almanac) (getSeedRange x))) (seeds almanac))
 
     getSeedRange :: SeedRange -> [Int]
-    getSeedRange seedRange = take (size seedRange - 1) (iterate (+ 1) (startNo seedRange))
+    getSeedRange seedRange = take (size seedRange) (iterate (+ 1) (startNo seedRange))
+
+    findSeedForLowestLocation almanac = head (dropWhile
+        (\x -> (length (filter (\y -> seedIsInAlmanac y almanac) (findSeedsForLocation almanac x))) == 0)
+        (iterate (+ 1) 0))
 
     findLocationForSeed :: Almanac -> Int -> Int
     findLocationForSeed almanac seedNo =
@@ -23,7 +27,22 @@ module Day5.Day5 where
         $ lookupInAlmanac almanac waterToLight
         $ lookupInAlmanac almanac fertilizerToWater
         $ lookupInAlmanac almanac soilToFertilizer
-        $ lookupInAlmanac almanac seedToSoil seedNo
+        $ lookupInAlmanac almanac seedToSoil
+        seedNo
+
+    findSeedsForLocation :: Almanac -> Int -> [Int]
+    findSeedsForLocation almanac locationNo =
+        reverseLookupInAlmanac almanac seedToSoil
+        $ reverseLookupInAlmanac almanac soilToFertilizer
+        $ reverseLookupInAlmanac almanac fertilizerToWater
+        $ reverseLookupInAlmanac almanac waterToLight
+        $ reverseLookupInAlmanac almanac lightToTemperature
+        $ reverseLookupInAlmanac almanac temperatureToHumidity
+        $ reverseLookupInAlmanac almanac humidityToLocation
+        [locationNo]
+
+    seedIsInAlmanac :: Int -> Almanac -> Bool
+    seedIsInAlmanac seedNo almanac = 1 == (length (filter (\x -> and [seedNo < ((startNo x) + (size x)), seedNo >= (startNo x)]) (seeds almanac)))
 
     lookupInAlmanac :: Almanac -> (Almanac -> [Map]) -> Int -> Int
     lookupInAlmanac almanac function value = if (length correctMap == 1)
@@ -32,9 +51,26 @@ module Day5.Day5 where
         where
             correctMap = filter (\x -> mapMapsValue x value) (function almanac)
 
+    reverseLookupInAlmanac :: Almanac -> (Almanac -> [Map]) -> [Int] -> [Int]
+    reverseLookupInAlmanac almanac function values = concat (map (reverseLookupSingleValueInAlmanac almanac function) values)
+
+    reverseLookupSingleValueInAlmanac :: Almanac -> (Almanac -> [Map]) -> Int -> [Int]
+    reverseLookupSingleValueInAlmanac almanac function value = if (length correctMaps == 0)
+        then [value]
+        else (reverseLookupValueInMaps value correctMaps)
+        where
+            correctMaps = filter(\x -> mapReverseMapsValue x value) (function almanac)
+
     mapMapsValue :: Map -> Int -> Bool
     mapMapsValue lookupMap value = and [
         value >= (sourceStart lookupMap),
         value <= ((sourceStart lookupMap) + (rangeSize lookupMap))]
 
-    lookupValueInMap value lookupMap = value - (sourceStart lookupMap) + (destStart lookupMap) where
+    mapReverseMapsValue :: Map -> Int -> Bool
+    mapReverseMapsValue lookupMap value = and [
+        value >= (destStart lookupMap),
+        value <= ((destStart lookupMap) + (rangeSize lookupMap))]
+
+    lookupValueInMap value lookupMap = value - (sourceStart lookupMap) + (destStart lookupMap)
+
+    reverseLookupValueInMaps value lookupMaps = map (\m -> value - (destStart m) + (sourceStart m)) lookupMaps
