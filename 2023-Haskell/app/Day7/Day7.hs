48dfdef53d0966b1c2e8f58c2c1b08d20acd3b2f
Chris Emerson <git@chris.emerson.email>
2023-12-07 21:40:18 +0000

    Day 7 part 2

--- 2023-Haskell/app/Day7/Day7.hs
+++ 2023-Haskell/app/Day7/Day7.hs
@@ -7,7 +7,7 @@ module Day7.Day7 where
     part1 :: String -> String
     part2 :: String -> String
 
-    data Hand = Hand { cards :: String, bid :: Int } deriving (Show, Eq)
+    data Hand = Hand { cards :: String, bid :: Int, jokers :: Bool } deriving (Show, Eq)
 
     instance Ord Hand where
         compare a b = if handType (scoreHand a) == handType (scoreHand b)
@@ -18,8 +18,8 @@ module Day7.Day7 where
         deriving (Enum, Show, Ord, Eq)
     data HandScore = HandScore { handType :: HandType, ranking :: [Int] } deriving (Show)
 
-    part1 input = show $ getWinnings (parseInput input)
-    part2 input = input
+    part1 input = show $ getWinnings (parseInput False input)
+    part2 input = show $ getWinnings (parseInput True input)
 
     getWinnings hands = ifoldl (\a i h -> a + ((bid h) * (i + 1))) 0 sortedHands where
         sortedHands = fromList (sort hands)
@@ -31,12 +31,14 @@ module Day7.Day7 where
             then compareRankings (tail a) (tail b)
         else compare a b
 
-    parseInput input = map (parseHand . trim) (lines input)
+    parseInput useJokers input = map ((parseHand useJokers) . trim) (lines input)
 
-    parseHand line = Hand { cards = (head handParts), bid = read (head (tail handParts)) } where
+    parseHand useJokers line = Hand { cards = (head handParts), bid = read (head (tail handParts)), jokers = useJokers } where
         handParts = splitOn " " line
 
-    scoreHand hand = HandScore { handType = getHandType (cards hand), ranking = map getCardRankingFromCard (cards hand) }
+    scoreHand hand = HandScore { handType = handTypeFn (cards hand), ranking = map cardRankingFn (cards hand) } where
+        cardRankingFn = if (jokers hand) then getCardRankingWithJokers else getCardRanking
+        handTypeFn = if (jokers hand) then getHandTypeWithJokers else getHandType
 
     getHandType cards =
         if hasFiveOfAKind cards
@@ -54,16 +56,56 @@ module Day7.Day7 where
         else
             HighCard
 
+    getHandTypeWithJokers cards =
+        if hasFiveOfAKind cards
+            then FiveOfAKind
+        else if hasFourOfAKind cards
+            then if or [hasOneJoker cards, hasFourJokers cards]
+                then FiveOfAKind
+                else FourOfAKind
+        else if and [hasThreeOfAKind cards, hasPair cards]
+            then if or [hasTwoJokers cards, hasThreeJokers cards]
+                then FiveOfAKind
+                else FullHouse
+        else if hasThreeOfAKind cards
+            then if hasThreeJokers cards
+                then FourOfAKind
+            else if hasOneJoker cards
+                then FourOfAKind
+                else ThreeOfAKind
+        else if hasTwoPair cards
+            then if hasTwoJokers cards
+                then FourOfAKind
+            else if hasOneJoker cards
+                then FullHouse
+                else TwoPair
+        else if hasPair cards
+            then if or [hasOneJoker cards, hasTwoJokers cards]
+                then ThreeOfAKind
+            else Pair
+        else if hasOneJoker cards
+            then Pair
+        else HighCard
+
     hasFiveOfAKind cards = length (filter (\f -> snd f == 5) (frequencies cards)) == 1
     hasFourOfAKind cards = length (filter (\f -> snd f == 4) (frequencies cards)) == 1
     hasThreeOfAKind cards = length (filter (\f -> snd f == 3) (frequencies cards)) == 1
     hasTwoPair cards = length (filter (\f -> snd f == 2) (frequencies cards)) == 2
     hasPair cards = length (filter (\f -> snd f == 2) (frequencies cards)) == 1
 
-    getCardRankingFromCard card = case card of
+    hasOneJoker cards = length (filter (\f -> and [fst f == 'J', snd f == 1]) (frequencies cards)) == 1
+    hasTwoJokers cards = length (filter (\f -> and [fst f == 'J', snd f == 2]) (frequencies cards)) == 1
+    hasThreeJokers cards = length (filter (\f -> and [fst f == 'J', snd f == 3]) (frequencies cards)) == 1
+    hasFourJokers cards = length (filter (\f -> and [fst f == 'J', snd f == 4]) (frequencies cards)) == 1
+
+    getCardRanking card = case card of
         'A' -> 14
         'K' -> 13
         'Q' -> 12
         'J' -> 11
         'T' -> 10
         _ -> read [card]
+
+    getCardRankingWithJokers card = case card of
+        'J' -> 1
+        _ -> getCardRanking card
