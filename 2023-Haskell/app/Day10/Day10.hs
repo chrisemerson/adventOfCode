63454ccd47a9cbb9ae9c1ba0e5111188b74d64cc
Chris Emerson <git@chris.emerson.email>
2023-12-10 16:45:46 +0000

    Day 10 part 2

--- 2023-Haskell/app/Day10/Day10.hs
+++ 2023-Haskell/app/Day10/Day10.hs
@@ -1,5 +1,5 @@
 module Day10.Day10 where
-    import Data.Vector (fromList, head, Vector, ifoldl, ifilter, imap, map, empty)
+    import Data.Vector (fromList, head, Vector, ifoldl, ifilter, imap, map, empty, (//))
     import Util
 
     vmap = Data.Vector.map
@@ -18,7 +18,10 @@ module Day10.Day10 where
     part1 input = show $ findFurthestPipeDistance grid where
         grid = getInputGrid input
 
-    part2 input = input
+    part2 input = show $ findNoCellsInsidePipes gridWithoutStart pipeLocations where
+        pipeLocations = getPipeLocations grid
+        grid = getInputGrid input
+        gridWithoutStart = getGridWithoutStart grid
 
     getInputGrid :: String -> Vector (Vector Char)
     getInputGrid input = fromList (pmap (\x -> fromList (trim x)) (lines input))
@@ -31,8 +34,13 @@ module Day10.Day10 where
         directions = findDirections grid startPos
         startPos = findStart grid
 
-    findFurthestPipeDistance grid = dist (phead (dropWhile (not.scoutsInSamePlace) (iterate (\x -> advanceScouts x grid) initialState))) where
-        initialState = advanceScouts (getInitialScoutState grid) grid
+    findFurthestPipeDistance grid = dist (phead (dropWhile (not.scoutsInSamePlace) (iterateScoutPaths grid)))
+
+    iterateScoutPaths grid = iterate (\x -> advanceScouts x grid) (advanceScouts (getInitialScoutState grid) grid)
+
+    getPipeLocations grid =
+        [findStart grid]
+        ++ concat (pmap (\x -> [pos (scout1 x), pos (scout2 x)]) (take (findFurthestPipeDistance grid) (iterateScoutPaths grid)))
 
     scoutsInSamePlace scoutState = and [
         fst (pos (scout1 scoutState)) == fst (pos (scout2 scoutState)),
@@ -44,11 +52,13 @@ module Day10.Day10 where
         dist = (dist scoutState) + 1
     }
 
-    advanceScout scout grid = changeScoutDir (case dir scout of
+    advanceScout scout grid = changeScoutDir (changeScoutPos scout grid) grid
+
+    changeScoutPos scout grid = case dir scout of
         N -> Scout { pos = (fst (pos scout) - 1, snd (pos scout)), dir = dir scout }
         S -> Scout { pos = (fst (pos scout) + 1, snd (pos scout)), dir = dir scout }
         E -> Scout { pos = (fst (pos scout), snd (pos scout) + 1), dir = dir scout }
-        W -> Scout { pos = (fst (pos scout), snd (pos scout) - 1), dir = dir scout }) grid
+        W -> Scout { pos = (fst (pos scout), snd (pos scout) - 1), dir = dir scout }
 
     changeScoutDir scout grid = (case (getGridChar grid (fst (pos scout)) (snd (pos scout))) of
         '|' -> Scout { pos = pos scout, dir = currentDir }
@@ -58,6 +68,29 @@ module Day10.Day10 where
         'J' -> Scout { pos = pos scout, dir = if currentDir == S then W else N }
         'L' -> Scout { pos = pos scout, dir = if currentDir == S then E else N }) where
         currentDir = dir scout
+
+    findNoCellsInsidePipes :: Vector (Vector Char) -> [(Int, Int)] -> Int
+    findNoCellsInsidePipes grid pipeLocations = sum (imap (\i x -> findNoCellsInsidePipesOnLine x i pipeLocations) grid)
+
+    findNoCellsInsidePipesOnLine :: Vector Char -> Int -> [(Int, Int)] -> Int
+    findNoCellsInsidePipesOnLine line lineIndex pipeLocations =
+        fst (ifoldl (\a i x -> processCellForParity a i x lineIndex pipeLocations) (0, False) line)
+
+    processCellForParity :: (Int, Bool) -> Int -> Char -> Int -> [(Int, Int)] -> (Int, Bool)
+    processCellForParity state charIndex char lineIndex pipeLocations = (count, inside) where
+        currentInside = snd state
+        currentCount = fst state
+        inside = if and[cellPartOfPipes, or[char == '|', char == 'F', char == '7']] then not currentInside else currentInside
+        count = if and[not(cellPartOfPipes), inside] then currentCount + 1 else currentCount
+        cellPartOfPipes = cellIsPartOfPipes pipeLocations (lineIndex, charIndex)
+
+    getPipeCharAtCell :: Vector (Vector Char) -> [(Int, Int)] -> (Int, Int) -> Char
+    getPipeCharAtCell grid pipeLocations pos = if cellIsPartOfPipes pipeLocations pos
+        then getGridChar grid (fst pos) (snd pos)
+        else '.'
+
+    cellIsPartOfPipes :: [(Int, Int)] -> (Int, Int) -> Bool
+    cellIsPartOfPipes pipePositions pos = length (filter (\x -> and[fst x == fst pos, snd x == snd pos]) pipePositions) >= 1
 
     findStart :: Vector (Vector Char) -> (Int, Int)
     findStart grid = ifoldl (\a i x -> if x /= -1 then (i, x) else a) (-1, -1) (vmap findStartOnLine grid)
@@ -75,12 +108,29 @@ module Day10.Day10 where
 
     canGoNorth grid pos = or [pipe == '|', pipe == 'F', pipe == '7']
         where pipe = getGridChar grid (fst pos - 1) (snd pos)
+
     canGoSouth grid pos = or [pipe == '|', pipe == 'J', pipe == 'L']
         where pipe = getGridChar grid (fst pos + 1) (snd pos)
+
     canGoEast grid pos = or [pipe == '-', pipe == 'J', pipe == '7']
         where pipe = getGridChar grid (fst pos) (snd pos + 1)
+
     canGoWest grid pos = or [pipe == '-', pipe == 'F', pipe == 'L']
         where pipe = getGridChar grid (fst pos) (snd pos - 1)
+
+    getGridWithoutStart :: Vector (Vector Char) -> Vector (Vector Char)
+    getGridWithoutStart grid = newGrid where
+        originalStartLine = getByIndex grid (fst startPos)
+        startPos = findStart grid
+        newStartChar = case findDirections grid startPos of
+            (N, S) -> '|'
+            (N, E) -> 'L'
+            (N, W) -> 'J'
+            (S, E) -> 'F'
+            (S, W) -> '7'
+            (E, W) -> '-'
+        newStartLine = originalStartLine // [((snd startPos), newStartChar)]
+        newGrid = grid // [((fst startPos), newStartLine)]
 
     getGridChar :: Vector (Vector Char) -> Int -> Int -> Char
     getGridChar grid y x = getByIndex (getByIndex grid y) x
