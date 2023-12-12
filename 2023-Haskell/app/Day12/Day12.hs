2c492ed4e8962a570f1d1f241d85dfe04ee48593
Chris Emerson <chris.emerson@bjss.com>
2023-12-12 13:03:00 +0000

    Day 12 part 1

--- 2023-Haskell/app/Day12/Day12.hs
+++ 2023-Haskell/app/Day12/Day12.hs
@@ -1,6 +1,32 @@
 module Day12.Day12 where
+    import Data.List (group)
+    import Data.List.Split
+    import Util
+
     part1 :: String -> String
     part2 :: String -> String
 
-    part1 input = input
+    data SpringRow = SpringRow { row :: String, unknowns :: [Int], pattern :: [Int] } deriving (Show)
+
+    part1 input = show $ sum (map getValidSpringCombinations parsedInput) where
+        parsedInput = parseInput input
+
     part2 input = input
+
+    parseInput input = map parseLine (lines input)
+
+    parseLine line = SpringRow { row = head lineParts, unknowns = springUnknowns, pattern = springPattern } where
+        springPattern = map read (splitOn "," (head (tail lineParts)))
+        springUnknowns = findUnknowns (head lineParts)
+        lineParts = splitOn " " line
+
+    findUnknowns springs = snd (foldl (\a x -> if x == '?' then ((fst a) + 1, (snd a) ++ [fst a]) else ((fst a) + 1, snd a)) (0, []) springs)
+
+    getValidSpringCombinations :: SpringRow -> Int
+    getValidSpringCombinations springRow = if length (unknowns springRow) == 0
+        then if springRowIsValid springRow then 1 else 0
+        else getValidSpringCombinations SpringRow { row = replaceCharInString (row springRow) (head (unknowns springRow)) '#', unknowns = tail (unknowns springRow), pattern = (pattern springRow) }
+            + getValidSpringCombinations SpringRow { row = replaceCharInString (row springRow) (head (unknowns springRow)) '.', unknowns = tail (unknowns springRow), pattern = (pattern springRow) }
+
+    springRowIsValid :: SpringRow -> Bool
+    springRowIsValid springRow = pattern springRow == map length (filter (\x -> head x == '#') (group (row springRow)))
--- 2023-Haskell/app/Util.hs
+++ 2023-Haskell/app/Util.hs
@@ -11,6 +11,9 @@ module Util where
     stringEndsWith :: String -> String -> Bool
     stringEndsWith str search = (drop ((length str) - (length search)) str) == search
 
+    stringContains :: String -> String -> Bool
+    stringContains str search = if stringStartsWith str search then True else stringContains (drop 1 str) search
+
     range :: Int -> Int -> [Int]
     range start end = take (end - start + 1) (iterate (+ 1) start)
 
@@ -20,3 +23,6 @@ module Util where
     rmdups :: Eq a => [a] -> [a]
     rmdups [] = []
     rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+    replaceCharInString :: String -> Int -> Char -> String
+    replaceCharInString str idx char = (take idx str) ++ [char] ++ (drop (idx + 1) str)
