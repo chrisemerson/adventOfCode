a3443be86f40fc77f7e7f91941f998ec58037128
Chris Emerson <chris.emerson@bjss.com>
2023-12-14 14:45:19 +0000

    Day 14 part 1

--- 2023-Haskell/app/Day14/Day14.hs
+++ 2023-Haskell/app/Day14/Day14.hs
@@ -1,6 +1,36 @@
 module Day14.Day14 where
+    import Grid
+    import Util
+
     part1 :: String -> String
     part2 :: String -> String
 
-    part1 input = input
-    part2 input = input
+    part1 input = show $ calculateStress gridAfterRolls where
+        gridAfterRolls = rollRocksNorth grid
+        grid = convertToGrid input
+
+    part2 input = show $ swapGridChars grid (0, 0) (0, 1) where
+       grid = convertToGrid input
+
+    rollRocksNorth grid = rollRocks grid (-1) 0 findRocksThatCanRollNorth
+    rollRocksWest grid = rollRocks grid 0 (-1) findRocksThatCanRollNorth
+    rollRocksSouth grid = rollRocks grid 1 0 findRocksThatCanRollNorth
+    rollRocksEast grid = rollRocks grid 0 1 findRocksThatCanRollNorth
+
+    rollRocks :: Grid -> Int -> Int -> (Grid -> [(Int, Int)]) -> Grid
+    rollRocks grid yOffset xOffset findFn = if not (length (findFn grid) > 0)
+        then grid
+        else rollRocks (swapGridChars grid rockToMove (((fst rockToMove) + yOffset), ((snd rockToMove) + xOffset))) yOffset xOffset findFn where
+           rockToMove = head (findFn grid)
+
+    findRocks grid = filter (\c -> getGridChar grid (fst c) (snd c) == 'O') cells where
+        cells = concat $ map (\r -> map (\c -> (r, c)) (range 0 ((glength (ghead grid)) - 1))) (range 0 ((glength grid) - 1))
+
+    findRocksThatCanRollNorth grid = filter (\r -> and[fst r > 0, getGridChar grid ((fst r) - 1) (snd r) == '.']) (findRocks grid)
+    findRocksThatCanRollWest grid = filter (\r -> and[fst r > 0, getGridChar grid ((fst r) - 1) (snd r) == '.']) (findRocks grid)
+    findRocksThatCanRollSouth grid = filter (\r -> and[fst r > 0, getGridChar grid ((fst r) - 1) (snd r) == '.']) (findRocks grid)
+    findRocksThatCanRollEast grid = filter (\r -> and[fst r > 0, getGridChar grid ((fst r) - 1) (snd r) == '.']) (findRocks grid)
+
+    calculateStress grid = sum stressPerRow where
+        stressPerRow = gmap (\i r -> (gridHeight - i) * (glength (gfilter (\_ c -> c == 'O') r))) grid
+        gridHeight = glength grid
--- 2023-Haskell/app/Grid.hs
+++ 2023-Haskell/app/Grid.hs
@@ -1,12 +1,28 @@
 module Grid where
-    import Data.Vector (fromList, head, ifilter, Vector)
+    import Data.Vector (fromList, head, ifilter, Vector, imap, ifoldl, length, drop, take)
     import Util
 
-    convertToGrid :: String -> Vector (Vector Char)
+    type Grid = Vector (Vector Char)
+
+    gmap = Data.Vector.imap
+    gfilter = Data.Vector.ifilter
+    gfoldl = Data.Vector.ifoldl
+    glength = Data.Vector.length
+    ghead = Data.Vector.head
+    gdrop = Data.Vector.drop
+    gtake = Data.Vector.take
+
+    convertToGrid :: String -> Grid
     convertToGrid string = fromList (map (\x -> fromList (trim x)) (lines string))
 
-    getGridChar :: Vector (Vector Char) -> Int -> Int -> Char
+    getGridChar :: Grid -> Int -> Int -> Char
     getGridChar grid y x = getByIndex (getByIndex grid y) x
 
     getByIndex :: Vector a -> Int -> a
     getByIndex vector idx = Data.Vector.head (ifilter (\i x -> i == idx) vector)
+
+    swapGridChars :: Grid -> (Int, Int) -> (Int, Int) -> Grid
+    swapGridChars grid from to = newGrid where
+        newGrid = gmap (\y r -> gmap (\x c -> if (y, x) == from then originalToChar else if (y, x) == to then originalFromChar else c) r) grid
+        originalFromChar = getGridChar grid (fst from) (snd from)
+        originalToChar = getGridChar grid (fst to) (snd to)
