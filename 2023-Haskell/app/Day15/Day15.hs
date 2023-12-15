971f33b734d2bd535fea59165fc85b82d8a222e8
Chris Emerson <chris.emerson@bjss.com>
2023-12-15 10:15:05 +0000

    Day 15 part 1

--- 2023-Haskell/app/Day15/Day15.hs
+++ 2023-Haskell/app/Day15/Day15.hs
@@ -1,6 +1,12 @@
 module Day15.Day15 where
+    import Data.Char
+    import Data.List.Split
+    import Util
+
     part1 :: String -> String
     part2 :: String -> String
 
-    part1 input = input
+    part1 input = show $ sum (map hash (splitOn "," (trim input)))
     part2 input = input
+
+    hash str = (foldl (\a x -> (a + (ord x)) * 17) 0 str) `mod` 256
