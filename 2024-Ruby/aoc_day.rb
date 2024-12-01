585c1d918d9dea223d0f6fcd58d51baad4bbe143
Chris Emerson <git@chris.emerson.email>
2024-12-01 18:52:51 +0000

    Allow tests to return nil to disable running of them, in case of challenges that don't have a simple output as answer

--- 2024-Ruby/aoc_day.rb
+++ 2024-Ruby/aoc_day.rb
@@ -4,7 +4,7 @@ class AocDay
   def get_input = File.read('./' + self.class.name.to_s + '/input.txt')
   def get_test_input = File.read('./' + self.class.name.to_s + '/test.txt')
   def part1(input) = print "Part 1 Not yet implemented"
-  def part1_test_answer = print "Part 1 Test Solution not provided"
+  def part1_test_answer = print "Part 1 Test Solution not provided\n"
   def part2(input) = print "Part 2 Not yet implemented"
-  def part2_test_answer = print "Part 2 Test Solution not provided"
+  def part2_test_answer = print "Part 2 Test Solution not provided\n"
 end
