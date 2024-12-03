747a859125d6a1e4d2b5bcdc856fd2723f23906a
Chris Emerson <chris.emerson@bjss.com>
2024-12-03 10:02:25 +0000

    Day 3 part 1

---
+++ 2024-Ruby/Day3/day3.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Day3 < AocDay
+  def part1_test_answer = 161
+
+  def part2_test_answer = super
+
+  def part1(input) = input
+    .scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+    .map { |a, b| a.to_i * b.to_i }
+    .reduce(:+)
+
+  def part2(input)
+    super
+  end
+end
