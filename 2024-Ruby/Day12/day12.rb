9f78fed3cf82d02f701c2e2ac5a3af7636233442
Chris Emerson <git@chris.emerson.email>
2024-12-14 02:31:05 +0000

    Massively sped up parsing of input on day 12

--- 2024-Ruby/Day12/day12.rb
+++ 2024-Ruby/Day12/day12.rb
@@ -3,13 +3,24 @@
 class Day12 < AocDay
   def part1_test_answer = 1930
 
-  def part2_test_answer = super
+  def part2_test_answer = 1206
 
   def part1(input) = find_garden_areas(parse_input(input))
     .map { |ga| ga[:points].length * find_perimeter(ga[:points]) }
     .sum
 
-  def part2(input) = super
+  def part2(input)
+    garden = parse_input(input)
+
+    find_garden_areas(garden)
+      .map { |ga|
+
+        print "Area " + ga[:name] + " has " + count_edges(garden, ga[:points]).to_s + " edges\n"
+
+        ga[:points].length * count_edges(garden, ga[:points])
+      }
+      .sum
+  end
 
   private
 
@@ -23,20 +34,29 @@ class Day12 < AocDay
         .length
     }.sum
 
+  def count_edges(garden, points)
+    corners = 0
+
+
+
+    corners
+  end
+
   def find_garden_areas(garden)
     garden_areas = []
 
     garden_cells = garden
       .each_with_index
-      .reduce([]) { |acc, info| acc + info[0].each_with_index.map { |_, x| [info[1], x] } }
+      .reduce([]) { |acc, info| acc + info[0].each_with_index.map { |_, x| [info[1], x] } }.uniq
 
-    loop do
-      unassigned_cells = garden_cells.reject { |c| cell_in_garden_areas(garden_areas, c) }
+    unassigned_cells = garden_cells.reject { |c| garden_areas.map { |ga| ga[:points] }.flatten(1).include?(c) }.uniq
 
-      break if unassigned_cells.empty?
-
+    until unassigned_cells.empty?
       cell = unassigned_cells[0]
-      garden_areas << find_garden_area(garden, cell)
+      new_garden_area = find_garden_area(garden, cell)
+      garden_areas << new_garden_area
+
+      unassigned_cells = unassigned_cells.reject { |c| new_garden_area[:points].include?(c) }
     end
 
     garden_areas
@@ -44,38 +64,29 @@ class Day12 < AocDay
 
   def find_garden_area(garden, cell)
     cells_in_area = [cell]
-    cell_name = garden[cell[0]][cell[1]]
-    cell_assigned = true
 
-    unassigned_cells_of_same_letter = garden
-      .each_with_index
-      .map { |row, y| row.each_with_index.map { |_, x| [y, x] } }
-      .reduce([]) { |acc, row| acc + row.filter { |c| garden[c[0]][c[1]] == cell_name } }
+    loop do
+      new_cells = cells_in_area
+        .map { |c| adjacent_cells(garden, c) }
+        .flatten(1)
+        .uniq
+        .reject { |c| cells_in_area.include?(c) }
+        .filter { |c| garden[c[0]][c[1]] == garden[cell[0]][cell[1]] }
 
-    while cell_assigned
-      cell_assigned = false
+      break if new_cells.empty?
 
-      adjacent_cells = unassigned_cells_of_same_letter
-        .filter { |c| cells_in_area.filter { |cc| cc[0] == c[0] && cc[1] == c[1] }.empty? }
-        .filter { |c| cells_in_area.map { |cia| cells_adjacent(cia, c) }.any? }
-
-      unless adjacent_cells.empty?
-        cells_in_area += adjacent_cells
-        cell_assigned = true
-      end
+      cells_in_area = cells_in_area + new_cells
     end
 
-    { :name => garden[cell[0]][cell[1]], :points => cells_in_area.uniq }
+    { :name => garden[cell[0]][cell[1]], :points => cells_in_area }
   end
 
-  def cell_in_garden_areas(garden_areas, cell) = garden_areas
-    .map { |ga| ga[:points].map { |p| p[0] == cell[0] && p[1] == cell[1] }.any? }
-    .any?
+  def adjacent_cells(garden, cell) = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    .map { |dir| [cell[0] + dir[0], cell[1] + dir[1]] }
+    .reject { |c| c[0] < 0 || c[0] >= garden.length || c[1] < 0 || c[1] >= garden[0].length }
 
-  def cells_adjacent(cell1, cell2)
-    [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  def cells_adjacent(cell1, cell2) = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     .map { |dir| [cell1[0] + dir[0], cell1[1] + dir[1]] }
     .map { |c| c[0] == cell2[0] && c[1] == cell2[1] }
     .any?
-    end
 end
