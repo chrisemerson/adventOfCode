197c38cf2ca1fa21ed4b0a75a7a058e49b7aa28c
Chris Emerson <git@chris.emerson.email>
2024-12-15 23:44:22 +0000

    Day 15 part 2

--- 2024-Ruby/Day15/day15.rb
+++ 2024-Ruby/Day15/day15.rb
@@ -2,17 +2,19 @@
 
 class Day15 < AocDay
   def part1_test_answer = 10092
-  def part2_test_answer = super
+  def part2_test_answer = 9021
 
   def part1(input)
     room, moves = parse_input(input)
 
-    moves.reduce(room) { |acc, move| move_robot(acc, move) }
-      .each_with_index.map { |row, y| row
-      .each_with_index
-      .map { |cell, x|
-        { :y => y, :x => x, :c => cell }
-      }
+    moves
+      .reduce(room) { |acc, move| move_robot(acc, move, method(:move_grid_piece), method(:robot_can_move)) }
+      .each_with_index.map { |row, y|
+      row
+        .each_with_index
+        .map { |cell, x|
+          { :y => y, :x => x, :c => cell }
+        }
     }
       .flatten(1)
       .filter { |c| c[:c] == 'O' }
@@ -21,7 +23,21 @@ class Day15 < AocDay
   end
 
   def part2(input)
-    super
+    room, moves = parse_input(input)
+
+    room = transform_room_for_part_2(room)
+
+    moves.reduce(room) { |acc, move| move_robot(acc, move, method(:move_grid_piece_pt2), method(:piece_can_move)) }
+      .each_with_index.map { |row, y| row
+      .each_with_index
+      .map { |cell, x|
+        { :y => y, :x => x, :c => cell }
+      }
+    }
+      .flatten(1)
+      .filter { |c| c[:c] == '[' }
+      .map { |c| c[:y] * 100 + c[:x] }
+      .sum
   end
 
   private
@@ -32,7 +48,16 @@ class Day15 < AocDay
     [room.lines.map { |l| l.strip.split("") }, moves.lines.map { |l| l.strip.split("") }.flatten]
   end
 
-  def move_robot(room, move)
+  def transform_room_for_part_2(room) = room.map do |l|
+    l.join('')
+     .gsub('#', '##')
+     .gsub('.', '..')
+     .gsub('@', '@.')
+     .gsub('O', '[]')
+     .split('')
+  end
+
+  def move_robot(room, move, move_callback, can_move_callback)
     case move
     when '^'
       dir = [-1, 0]
@@ -46,14 +71,14 @@ class Day15 < AocDay
       raise ArgumentError.new "Invalid move instruction: #{move}"
     end
 
-    room = move_grid_piece(room, get_robot_pos(room), dir) if robot_can_move(room, dir)
+    if can_move_callback.call(room, get_robot_pos(room), dir)
+      room = move_callback.call(room, get_robot_pos(room), dir)
+    end
 
     room
   end
 
-  def robot_can_move(room, dir)
-    pos = get_robot_pos(room)
-
+  def robot_can_move(room, pos, dir)
     while room[pos[0]][pos[1]] != '#'
       pos = [pos[0] + dir[0], pos[1] + dir[1]]
 
@@ -63,10 +88,28 @@ class Day15 < AocDay
     false
   end
 
+  def piece_can_move(room, pos, dir)
+    cur_piece = room[pos[0]][pos[1]]
+    dest_pos = [pos[0] + dir[0], pos[1] + dir[1]]
+
+    return true if cur_piece == '.'
+    return false if cur_piece == '#'
+
+    return piece_can_move(room, dest_pos, dir) if cur_piece == '@'
+    return piece_can_move(room, dest_pos, dir) if dir == [0, 1] && %w([ ]).include?(cur_piece)
+    return piece_can_move(room, dest_pos, dir) if dir == [0, -1] && %w([ ]).include?(cur_piece)
+
+    dest_pos_arr = []
+
+    dest_pos_arr << dest_pos
+    dest_pos_arr << [dest_pos[0], dest_pos[1] + 1] if cur_piece == '['
+    dest_pos_arr << [dest_pos[0], dest_pos[1] - 1] if cur_piece == ']'
+
+    dest_pos_arr.map { |dp| piece_can_move(room, dp, dir) }.all?
+  end
+
   def move_grid_piece(room, pos, dir)
     destination_cell = room[pos[0] + dir[0]][pos[1] + dir[1]]
-
-    return room if destination_cell == '#'
 
     if destination_cell != '.'
       room = move_grid_piece(room, [pos[0] + dir[0], pos[1] + dir[1]], dir)
@@ -76,6 +119,45 @@ class Day15 < AocDay
     room[pos[0]][pos[1]] = '.'
 
     room
+  end
+
+  def move_grid_piece_pt2(room, pos, dir)
+    return room if room[pos[0]][pos[1]] == '.'
+
+    object_to_move = [pos]
+
+    if room[pos[0]][pos[1]] == ']'
+      object_to_move << [pos[0], pos[1] - 1]
+    elsif room[pos[0]][pos[1]] == '['
+      object_to_move << [pos[0], pos[1] + 1]
+    end
+
+    if [[1, 0], [-1, 0]].include?(dir)
+      destination_cells = object_to_move.map { |p| [p[0] + dir[0], p[1] + dir[1]] }
+      left_behind_cells = object_to_move.map(&:itself)
+    elsif dir == [0, 1]
+      destination_cells = [[object_to_move[0][0], object_to_move.map { |p| p[1] }.max + 1]]
+      left_behind_cells = [[object_to_move[0][0], object_to_move.map { |p| p[1] }.min]]
+    else
+      destination_cells = [[object_to_move[0][0], object_to_move.map { |p| p[1] }.min - 1]]
+      left_behind_cells = [[object_to_move[0][0], object_to_move.map { |p| p[1] }.max]]
+    end
+
+    if destination_cells.map { |dc| room[dc[0]][dc[1]] != '.' }.any?
+      room = destination_cells.reduce(room) { |acc, cur|
+        move_grid_piece_pt2(acc, cur, dir)
+      }
+    end
+
+    room = object_to_move.reduce(Marshal.load(Marshal.dump(room))) { |acc, cur|
+      acc[cur[0] + dir[0]][cur[1] + dir[1]] = room[cur[0]][cur[1]]
+      acc
+    }
+
+    left_behind_cells.reduce(Marshal.load(Marshal.dump(room))) { |acc, cur|
+      acc[cur[0]][cur[1]] = '.'
+      acc
+    }
   end
 
   def get_robot_pos(room) = room
@@ -88,12 +170,4 @@ class Day15 < AocDay
         }.reject(&:nil?)
     }.flatten(1)
     .reject(&:nil?)[0]
-
-  def print_room(room)
-    room.each do |line|
-      print line.join('') + "\n"
-    end
-
-    print "\n"
-  end
 end
