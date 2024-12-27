fa3e02e4c72d3424c68b02523af5de26cbba6194
Chris Emerson <git@chris.emerson.email>
2024-12-27 23:54:28 +0000

    Day 24 part 2 code to help debug adder

--- 2024-Ruby/Day24/day24.rb
+++ 2024-Ruby/Day24/day24.rb
@@ -2,7 +2,7 @@
 
 class Day24 < AocDay
   def part1_test_answer = 2024
-  def part2_test_answer = 'z00,z01,z02,z05'
+  def part2_test_answer = nil
 
   def part1(input)
     inputs, gates = parse_input(input)
@@ -13,31 +13,76 @@ class Day24 < AocDay
   def part2(input)
     inputs, gates = parse_input(input)
 
-    x = inputs.keys.filter { |g| g[0] == 'x' }.map { |g| get_value(g, inputs, gates) * (2 ** g[1..].to_i) }.sum
-    y = inputs.keys.filter { |g| g[0] == 'y' }.map { |g| get_value(g, inputs, gates) * (2 ** g[1..].to_i) }.sum
-    z = gates.keys.filter { |g| g[0] == 'z' }.map { |g| get_value(g, inputs, gates) * (2 ** g[1..].to_i) }.sum
+    num_len = inputs.keys.filter { |g| g[0] == 'x' }.length
 
+    swaps = [%w[z09 gwh], %w[wgb wbw], %w[z21 rcb], %w[jct z39]]
+    ignore = ['vvc']
+    start_at = 22
 
-    print x.to_s + " + " + y.to_s + " = " + z.to_s + "\n"
+    swaps.each do |(x, y)|
+      temp = gates[x]
+      gates[x] = gates[y]
+      gates[y] = temp
+    end
 
-    'z00,z01,z02,z05'
+    (start_at..num_len).each do |i|
+      x = 2 ** i
+      y = 2 ** i
+
+      inputs = x.to_s(2).rjust(num_len, '0').reverse.split('').each_with_index
+                .reduce({}) { |acc, (c, i)| acc.merge({ 'x' + i.to_s.rjust(2, '0') => c.to_i }) }
+
+      inputs = y.to_s(2).rjust(num_len, '0').reverse.split('').each_with_index
+                .reduce(inputs) { |acc, (c, i)| acc.merge({ 'y' + i.to_s.rjust(2, '0') => c.to_i }) }
+
+      begin
+        x = inputs.keys.filter { |g| g[0] == 'x' }.map { |g| get_value(g, inputs, gates) * (2 ** g[1..].to_i) }.sum
+        y = inputs.keys.filter { |g| g[0] == 'y' }.map { |g| get_value(g, inputs, gates) * (2 ** g[1..].to_i) }.sum
+        z = gates.keys.filter { |g| g[0] == 'z' }.map { |g| get_value(g, inputs, gates) * (2 ** g[1..].to_i) }.sum
+      rescue ArgumentError
+        next
+      end
+
+      print x.to_s + " + " + y.to_s + " = " + z.to_s + "\n"
+
+      unless x + y == z
+        begin
+          get_value('z' + i.to_s.rjust(2, '0'), inputs, gates, true, '', ignore).to_s
+          gates_to_ignore = STDIN.gets
+          ignore = [*ignore, *gates_to_ignore.split(',').map(&:strip)]
+        rescue ArgumentError
+          next
+        end
+      end
+    end
+
+    swaps.flatten.uniq.sort.join(',')
   end
 
   private
 
-  def get_value(name, inputs, gates)
+  def get_value(name, inputs, gates, trace = false, indent = "", ignore = [])
+    if ignore.include?(name)
+      trace = false
+    end
+
     return inputs[name] if inputs.has_key?(name)
+
     raise ArgumentError.new "No such value #{name}" unless gates.has_key?(name)
 
     gate = gates[name]
 
+    if trace
+      print indent + name + ": " + gate[:a] + " " + gate[:operator].to_s + " " + gate[:b] + "\n"
+    end
+
     case gate[:operator]
     when :OR
-      return get_value(gate[:a], inputs, gates) | get_value(gate[:b], inputs, gates)
+      get_value(gate[:a], inputs, gates, trace, indent + "  ", ignore) | get_value(gate[:b], inputs, gates, trace, indent + "  ", ignore)
     when :AND
-      return get_value(gate[:a], inputs, gates) & get_value(gate[:b], inputs, gates)
+      get_value(gate[:a], inputs, gates, trace, indent + "  ", ignore) & get_value(gate[:b], inputs, gates, trace, indent + "  ", ignore)
     when :XOR
-      return get_value(gate[:a], inputs, gates) ^ get_value(gate[:b], inputs, gates)
+      get_value(gate[:a], inputs, gates, trace, indent + "  ", ignore) ^ get_value(gate[:b], inputs, gates, trace, indent + "  ", ignore)
     else
       raise ArgumentError.new "Unknown operator #{gate[:operator]}"
     end
