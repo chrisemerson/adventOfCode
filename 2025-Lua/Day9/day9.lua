95b061f723ad9c05e904ef111440e6fc243de71e
Chris Emerson <git@chris.emerson.email>
2025-12-13 23:21:21 +0000

    Refactor and comment

--- 2025-Lua/Day9/day9.lua
+++ 2025-Lua/Day9/day9.lua
@@ -49,35 +49,34 @@ local function point_is_on_line(point, line1, line2)
 end
 
 local function rectangle_intersects(p1, p2, p3, p4)
-    --First, just check if there are any points fully inside the rectangle
+    --Normalise the points
     local top_left = {['y'] = math.min(p1['y'], p2['y'], p3['y'], p4['y']), ['x'] = math.min(p1['x'], p2['x'], p3['x'], p4['x'])}
     local bottom_right = {['y'] = math.max(p1['y'], p2['y'], p3['y'], p4['y']), ['x'] = math.max(p1['x'], p2['x'], p3['x'], p4['x'])}
     local top_right = {['y'] = math.min(p1['y'], p2['y'], p3['y'], p4['y']), ['x'] = math.max(p1['x'], p2['x'], p3['x'], p4['x'])}
     local bottom_left = {['y'] = math.max(p1['y'], p2['y'], p3['y'], p4['y']), ['x'] = math.min(p1['x'], p2['x'], p3['x'], p4['x'])}
 
+    --First, just check if there are any points fully inside the rectangle
     for _, p in ipairs(points) do
         if p['x'] > top_left['x'] and p['x'] < bottom_right['x'] and p['y'] > top_left['y'] and p['y'] < bottom_right['y'] then
             return true
         end
     end
 
+    --Loop through line segments and check if there's an intersection with the rectangle
     local lp = points[#points]
 
     for _, p in ipairs(points) do
         -- Account for the case where the point is on a line and the next point is outside the shape but taking the line
-        -- through the shape
+        -- through the shape. Also check here for simple intersections
 
         if (point_is_on_line(lp, top_left, top_right) and p['y'] > top_left['y'])
         or (point_is_on_line(lp, bottom_left, bottom_right) and p['y'] < top_left['y'])
         or (point_is_on_line(lp, top_left, bottom_left) and p['x'] > top_left['x'])
-        or (point_is_on_line(lp, top_right, bottom_right) and p['x'] < top_right['x']) then
-            return true
-        end
-
-        if line_intersects(top_left, top_right, lp, p)
-            or line_intersects(top_right, bottom_right, lp, p)
-            or line_intersects(bottom_left, bottom_right, lp, p)
-            or line_intersects(top_left, bottom_left, lp, p) then
+        or (point_is_on_line(lp, top_right, bottom_right) and p['x'] < top_right['x'])
+        or line_intersects(top_left, top_right, lp, p)
+        or line_intersects(top_right, bottom_right, lp, p)
+        or line_intersects(bottom_left, bottom_right, lp, p)
+        or line_intersects(top_left, bottom_left, lp, p) then
             return true
         end
 
@@ -89,11 +88,15 @@ end
 
 max_area = 0
 
+-- Loop through all possible rectangles between 2 points in the set
 for _, i in ipairs(points) do
     for _, j in ipairs(points) do
+        -- Calculate area
         local this_area = (math.abs(i['x'] - j['x']) + 1) * (math.abs(i['y'] - j['y']) + 1)
 
+        -- If it's not bigger than the current biggest area we have, no point going through the expensive validation
         if this_area > max_area then
+            --Work out the 2 other points in the rectangle
             local k = {['y'] = i['y'], ['x'] = j['x']}
             local l = {['y'] = j['y'], ['x'] = i['x']}
 
